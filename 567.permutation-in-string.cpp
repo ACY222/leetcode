40aeb5319a4aa5cc211b1f5833db7d32e8a75a46
ACY222 <838113915@qq.com>
2025-08-17 10:07:29 +0800

    567 version 1.2
    using vector rather than map, we can even use array.
    And the performance is unbelievable, it reminds me that if vector is
    enough, don't use map or set instead

--- 567.permutation-in-string.cpp
+++ 567.permutation-in-string.cpp
@@ -1,9 +1,9 @@
 // @leet start
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 using std::string;
-using std::unordered_map;
+using std::vector;
 
 class Solution {
 public:
@@ -13,26 +13,24 @@ public:
     }
     // the length of the substring is s1.size(), so only the offset matters
     // need contains what we want, and window contains what we have
-    unordered_map<char, int> need {}, window {};
+    vector<int> need(26, 0), window(26, 0);
     int left {0}, right {0};
-    // initialize the map need and window
+    // initialize the vectors and then compare between them
     for (; right < s1.size(); ++right) {
-      ++need[s1[right]];
-      ++window[s2[right]];
-    }
-    for (; right < s2.size(); ++right, ++left) {
-      // if two map match, return true
-      if (need == window) {
-        return true;
-      }
-      ++window[s2[right]];
-      --window[s2[left]];
-      if (window[s2[left]] == 0) {
-        window.erase(s2[left]);
-      }
+      ++need[s1[right] - 'a'];
+      ++window[s2[right] - 'a'];
     }
     if (need == window) {
       return true;
+    }
+    // using the sliding window
+    for (; right < s2.size(); ++right, ++left) {
+      // move window forward
+      ++window[s2[right] - 'a'];
+      --window[s2[left] - 'a'];
+      if (need == window) {
+        return true;
+      }
     }
     return false;
   }
