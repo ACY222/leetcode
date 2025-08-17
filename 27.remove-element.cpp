81359018a9f34521b5506374e7299ebf59f2459e
ACY222 <838113915@qq.com>
2025-08-17 11:52:40 +0800

    27 version 1.1
    use fast and slow pointers, where fast move forward, to solve it.
    this method is more clear and more elegant

--- 27.remove-element.cpp
+++ 27.remove-element.cpp
@@ -9,27 +9,15 @@ public:
     if (nums.empty()) {
       return 0;
     }
-    int slow {0}, fast {(int)nums.size() - 1};
-    while (fast > slow) {
+    int slow {0}, fast {0};
+    while (fast < nums.size()) {
       // if we find a val in nums, we need to find another element to replace
-      if (nums[slow] == val) {
-        // so we keep moving fast backward to find an element
-        while (fast > slow && nums[fast] == val) {
-          --fast;
-        }
-        // if fast > slow, it means we find that, assign it to nums[slow]
-        if (fast > slow) {
-          nums[slow] = nums[fast];
-          --fast;
-          continue;
-        }
-        // otherwise, fast <= slow
-        break;
+      if (nums[fast] != val) {
+        nums[slow++] = nums[fast];
       }
-      // keep moving slow forward
-      ++slow;
+      ++fast;
     }
-    return nums[slow] == val ? slow : slow + 1;
+    return slow;
   }
 };
 // @leet end
