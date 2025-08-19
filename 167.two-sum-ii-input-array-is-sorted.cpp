a4601012b7278eb0ed3fbd3c6d9dfbf236f60998
ACY222 <838113915@qq.com>
2025-08-19 09:30:30 +0800

    167. version 2.0
    Using left and right pointers to solve it

--- 167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted.cpp
@@ -1,29 +1,23 @@
 // @leet start
 #include <vector>
-#include <unordered_map>
 
 using std::vector;
-using std::unordered_map;
 
 class Solution {
 public:
   vector<int> twoSum(vector<int>& numbers, int target) {
-    int first {0}, second {1};
-    unordered_map<int, int> numToIndex { {numbers[0], 0} };
-    while (second < numbers.size()) {
-      // if the number at position second is new
-      if (numbers[second] != numbers[first]) {
-        // try to find another part in the map
-        auto iter = numToIndex.find(target - numbers[second]);
-        // if we can find, return two indices
-        if (iter != numToIndex.end()) {
-          return {iter->second + 1, second + 1};
-        }
-        // and then insert new value into the map, and update first
-        numToIndex.insert({numbers[second], second});
-        first = second;
+    int left {0}, right {(int)numbers.size() - 1};
+    while (left < right) {
+      int sum { numbers[left] + numbers[right] };
+      if (sum == target) {
+        return { left + 1, right + 1 };
       }
-      ++second;
+      else if (sum < target) {
+        ++left;
+      }
+      else {
+        --right;
+      }
     }
     return {};
   }
