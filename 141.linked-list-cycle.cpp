c826ac5cdbfc1823d535e548c283b08404819d35
ACY222 <838113915@qq.com>
2025-08-20 10:39:24 +0800

    141. version 2.0
    I use set to record the node we have found. If we find a node twice,
    then there is a cycle. It works but it's slow. Because the time
    complexity is O(nlogn), and space complexity is O(n). Meanwhile, the
    fast and slow pointers are O(n) and O(1)

--- 141.linked-list-cycle.cpp
+++ 141.linked-list-cycle.cpp
@@ -7,16 +7,27 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+#include <set>
+
+using std::set;
+
 class Solution {
 public:
   bool hasCycle(ListNode *head) {
-    ListNode *fast {head}, *slow {head};
-    while (fast != nullptr && fast->next != nullptr) {
-      fast = fast->next->next;
-      slow = slow->next;
-      if (fast == slow) {
+    // record the listnode we find
+    set<ListNode*> nodeWeFound {};
+    ListNode *p {head};
+    while (p != nullptr) {
+      // if we have found this node before
+      // we find it twice so there must be cycles
+      // but how to compare two node? by their val and next?
+      // then how to compare the next
+      // countless recursion!
+      if (nodeWeFound.count(p)) {
         return true;
       }
+      nodeWeFound.insert(p);
+      p = p->next;
     }
     return false;
   }
