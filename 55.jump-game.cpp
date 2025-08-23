2008d9da9f018bc87fe291f4e645b04b144cada5
ACY222 <838113915@qq.com>
2025-08-23 09:18:10 +0800

    55. version 2.0
    A method learnt from leetcode. More concise, more elegant, but no better
    performance compared to my solution

--- 55.jump-game.cpp
+++ 55.jump-game.cpp
@@ -1,30 +1,19 @@
 // @leet start
+#include <algorithm>
 #include <vector>
 using std::vector;
+using std::max;
 
 class Solution {
 public:
   bool canJump(vector<int>& nums) {
-    for (int index = 0; index < nums.size(); ) {
-      // we can jump to the end directly
-      if (index + nums[index] >= nums.size() - 1) {
-        return true;
-      }
-      // try to move as far as possible
-      int maxStep {index + nums[index]}, nextIndex {index};
-      for (int i = 1; i <= nums[index]; ++i) {
-        if (nums[index + i] + index + i > maxStep) {
-          maxStep = nums[index + i] + index + i;
-          nextIndex = index + i;
-        }
-      }
-      // if we cannot move forward
-      if (index >= nextIndex) {
-        return false;
-      }
-      index = nextIndex;
+    int maxReach {0};
+    for (int i = 0; i < nums.size(); ++i) {
+      // if we cannot reach i-th element
+      if (i > maxReach) { return false; };
+      maxReach = max(maxReach, i + nums[i]);
     }
-    return false;
+    return true;
   }
 };
 // @leet end
