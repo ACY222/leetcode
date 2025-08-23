d6358a799bdc2e044aafd19f65dac67b244ad3c5
ACY222 <838113915@qq.com>
2025-08-23 13:07:14 +0800

    1306. version 1.1
    Improve it by avoid some of the recursions, but the performance is even
    worse

--- 1306.jump-game-iii.cpp
+++ 1306.jump-game-iii.cpp
@@ -6,18 +6,31 @@ using std::unordered_map;
 class Solution {
 public:
   bool canReach(vector<int>& arr, int start) {
-    // we have reached this element and failed
+    // we have reached this element and failed, now we reach it again, so failed
     if (flag[start] == true) {
       return false;
     }
     flag[start] = true;
+    // no, I should solve this in above layer, so it won't overflow
     if (start < 0 || start >= arr.size()) {
       return false;
     }
+    // if the value is 0, we have reached the destination
     if (arr[start] == 0) {
       return true;
     }
-    return canReach(arr, start - arr[start]) || canReach(arr, start + arr[start]);
+    if (start - arr[start] < 0) {
+      if (start + arr[start] >= arr.size()) {
+        return false;
+      }
+      return canReach(arr, start + arr[start]);
+    }
+    else {
+      if (start + arr[start] >= arr.size()) {
+        return canReach(arr, start - arr[start]);
+      }
+      return canReach(arr, start - arr[start]) || canReach(arr, start + arr[start]);
+    }
   }
 private:
   // using this map to record if we have reached this element, so that we can
