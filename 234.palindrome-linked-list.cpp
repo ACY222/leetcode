63bd1a592149b96756560d5ad941cc2dcab4fbd1
ACY222 <838113915@qq.com>
2025-08-28 09:43:59 +0800

    234. version 2.0
    Reverse the second half part of the linked list so that we can compare
    the values from both sides with O(1) space.
    The space complexity is good, but speed is not

--- 234.palindrome-linked-list.cpp
+++ 234.palindrome-linked-list.cpp
@@ -9,45 +9,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
 #include <stack>
 using std::stack;
 class Solution {
 public:
   // in O(n) time and O(n) space, how to improve it
-  // Note that the node.val is in [0, 9], we can store it as a digit of int
-  // But the number of nodes is in the range [1, 10^5], MAX_INT = 2.1 * 10^9
+  // we can reverse the back half linked list, and compare them from both sides
   bool isPalindrome(ListNode* head) {
-    if (!head->next) {    // if there is only one node, it's always true
-      return true;
-    }
-    stack<int> st;
-    ListNode *fast { head }, *slow { head };
-    // If head.size is even, then the first half vals are pushed, and fast ==
-    // nullptr
-    // if it's odd, then the first half(without the middle one) are pushed, and
-    // fast != nullptr
+    ListNode *fast { head }, *slow { head }, *prev, *temp;
+    // move fast to the end to find the node in the middle
     while (fast && fast->next) {
-      st.push(slow->val);
       fast = fast->next->next;
       slow = slow->next;
     }
-    if (fast) {   // if the number of the nodes in list is odd
-      // slow need to move forward
-      slow = slow->next;
-    }
+    // 1, 2, 3, 4, 5
+    //       |
+    // 1, 2, 3, 4
+    //       |
+    prev = slow;
+    slow = slow->next;
+    prev->next = nullptr;
+    // 1, 2, 3, 4, 5
+    //       p  s
+    // 1, 2, 3, 4
+    //       p  s
+    // begin to reverse the back half linked list
     while (slow) {
-      if (slow->val != st.top()) {
+      temp = slow->next;
+      slow->next = prev;
+      prev = slow;
+      slow = temp;
+    }
+    // after reversing the list, compare the node's val from both side
+    ListNode *begin { head }, *end { prev };
+    while (end) {
+      if (begin->val != end->val) {
         return false;
       }
-      slow = slow->next;
-      st.pop();
+      begin = begin->next;
+      end = end->next;
     }
     return true;
   }
