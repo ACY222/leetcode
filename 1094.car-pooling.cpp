9c5537c91272a2e0d70908c10e0bd69da25c3429
Ai Chang <ac@AideMacBook-Air.local>
2025-09-01 11:12:21 +0800

    1094 version 2.0
    Use difference array, and the performance improves greatly
    Performance: 100, 78

--- 1094.car-pooling.cpp
+++ 1094.car-pooling.cpp
@@ -3,16 +3,19 @@
 using std::vector;
 class Solution {
 public:
+  // the difference array helps
   bool carPooling(vector<vector<int>>& trips, int capacity) {
+    vector<int> diff(1001, 0);
     int n {(int)trips.size()};
-    vector<int> remainSeats(1000, capacity);
     for (int i = 0; i < n; ++i) {
       int num {trips[i][0]}, from {trips[i][1]}, to {trips[i][2]};
-      for (int j = from; j < to; ++j) {
-        if (remainSeats[j] < num) {
-          return false;
-        }
-        remainSeats[j] -= num;
+      diff[from] -= num;
+      diff[to] += num;
+    }
+    for (int i = 0; i <= 1000; ++i) {
+      capacity += diff[i];
+      if (capacity < 0) {
+        return false;
       }
     }
     return true;
