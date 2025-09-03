f7865f87d7cb47335bf88c7d58a4260ff0fbc580
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 11:02:05 +0800

    104 v2.0
    Use BFS to find the maxDepth instead of recursion.
    Performance: 100, 76. The performance increases slightly.

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -10,15 +10,30 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <algorithm>
-using std::max;
+#include <queue>
+using std::queue;
 class Solution {
 public:
+  // maybe I can use BFS to find the maxDepth
   int maxDepth(TreeNode* root) {
     if (!root) {  // base case: if root == nullptr
       return 0;
     }
-    return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    queue<TreeNode*> q {};
+    int depth {0};
+    q.push(root);
+    while (!q.empty()) {
+      TreeNode* top;
+      int size {(int)q.size()};
+      ++depth;
+      for (int i = 0; i < size; ++i) {
+        top = q.front();
+        q.pop();
+        if (top->left) { q.push(top->left); }
+        if (top->right) { q.push(top->right); }
+      }
+    }
+    return depth;
   }
 };
 // @leet end
