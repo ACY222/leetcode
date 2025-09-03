275c988ce08618f9ca91a5f375889eef9ee43dd0
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 20:28:57 +0800

    105 v1.1
    Use an unordered_map to record the index of each number in inorder to
    avoid looking for the indices again and again. And this does improve the
    efficiency
    Performance: 75, 68

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -10,32 +10,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <unordered_map>
 #include <vector>
 using std::vector;
+using std::unordered_map;
 class Solution {
 public:
   int count {0};
+  unordered_map<int, int> valueToIndex {};
   TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    return traverse(preorder, inorder.begin(), inorder.end());
+    int n = inorder.size();
+    for (int i = 0; i < n; ++i) {
+      valueToIndex[inorder[i]] = i;
+    }
+    return traverse(preorder, 0, n);
   }
 
 private:
   using iter = vector<int>::iterator;
   // input preorder and inorder(its begin and end iterator)
   // return the root of a organized tree
-  TreeNode* traverse(vector<int>& preorder, const iter begin, const iter end) {
-    if (begin == end) {
+  TreeNode* traverse(vector<int>& preorder, int begin, int end) {
+    if (begin >= end) {
       return nullptr;
     }
     TreeNode *curr = new TreeNode(preorder[count++]);
-    iter it;
-    for (it = begin; it != end; ++it) {
-      if (*it == curr->val) {
-        break;
-      }
-    }
-    TreeNode* left = traverse(preorder, begin, it);
-    TreeNode* right = traverse(preorder, it + 1, end);
+    int index {valueToIndex[curr->val]};
+    TreeNode* left = traverse(preorder, begin, index);
+    TreeNode* right = traverse(preorder, index + 1, end);
 
     curr->left = left;
     curr->right = right;
