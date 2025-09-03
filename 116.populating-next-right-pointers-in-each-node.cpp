6dd683eb6960eee3f56b80660a3311317f6755e2
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 19:17:02 +0800

    116 v1.0
    Interesting problem! Traverse all nodes and solve it by passing
    parameters
    Performance: 82, 93

---
+++ 116.populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,38 @@
+// @leet start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+class Solution {
+public:
+  Node* connect(Node* root) {
+    traverse(root, nullptr);
+    return root;
+  }
+private:
+  void traverse(Node* curr, Node* nextRight) {
+    if (!curr) {
+      return;
+    }
+    if (nextRight) {
+      curr->next = nextRight;
+    }
+    traverse(curr->left, curr->right);
+    traverse(curr->right, nextRight ? nextRight->left : nullptr);
+    return;
+  }
+};
+// @leet end
