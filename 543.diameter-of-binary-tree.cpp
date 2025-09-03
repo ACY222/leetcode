815c666edd1b357ccb868674f03ffe6d28f31dbb
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 11:25:14 +0800

    543 v1.1
    Use a map to record the depth of each node to avoid calculating it again
    and again
    Performance: 21, 5. Improves, but not greatly

--- 543.diameter-of-binary-tree.cpp
+++ 543.diameter-of-binary-tree.cpp
@@ -12,42 +12,35 @@
  */
 #include <queue>
 #include <algorithm>
+#include <unordered_map>
 using std::queue;
-using std::max;
+using std::max, std::unordered_map;
 class Solution {
 public:
+  // use the map so that we can calculate the depth of each node only once
+  unordered_map<TreeNode*, int> nodeToDepth {};
   int maxDistance {0};
   // input a root node, output maxDepth(root->left) + maxDepth(root->right)
   int diameterOfBinaryTree(TreeNode* root) {
     if (!root) {
       return 0;
     }
+    // I calculated the depth of the children nodes again and again
     maxDistance = max(maxDistance, maxDepth(root->left) + maxDepth(root->right));
-    maxDistance = max(maxDistance, diameterOfBinaryTree(root->left));
-    maxDistance = max(maxDistance, diameterOfBinaryTree(root->right));
-    return maxDistance;
+    return max(maxDistance, max(diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right)));
   }
 private:
   // return the maxDepth of the tree with root "root"
   int maxDepth(TreeNode* root) {
-    if (!root) {  // base case: if root == nullptr
+    // if we have already known the depth of root, return it
+    if (nodeToDepth.count(root)) {
+      return nodeToDepth[root];
+    }
+    if (!root) {    // base case, if root == nullptr
       return 0;
     }
-    queue<TreeNode*> q {};
-    int depth {0};
-    q.push(root);
-    while (!q.empty()) {
-      TreeNode* top;
-      int size {(int)q.size()};
-      ++depth;
-      for (int i = 0; i < size; ++i) {
-        top = q.front();
-        q.pop();
-        if (top->left) { q.push(top->left); }
-        if (top->right) { q.push(top->right); }
-      }
-    }
-    return depth;
+    nodeToDepth[root] = 1 + max(maxDepth(root->left), maxDepth(root->right));
+    return nodeToDepth[root];
   }
 };
 // @leet end
