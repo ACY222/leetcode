4f9e18d7c299f52c1881de413cea6d9c62788b77
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 12:05:27 +0800

    543 v1.3
    Remove the extra useless storage about depth and diameter, saving a lot
    of time and space
    Performance: 100, 72

--- 543.diameter-of-binary-tree.cpp
+++ 543.diameter-of-binary-tree.cpp
@@ -10,15 +10,13 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <queue>
 #include <algorithm>
-#include <unordered_map>
-using std::queue;
-using std::max, std::unordered_map, std::max_element;
+using std::max;
 class Solution {
 public:
   // use the map so that we can calculate the depth of each node only once
-  unordered_map<TreeNode*, int> nodeToDepth {}, nodeToDiameter {};
+  // Actually, I will only access the depth of each node once, so I shouldn't
+  // waste the space to do this
   int maxDiameter {0};
   // input a root node, output maxDepth(root->left) + maxDepth(root->right)
   int diameterOfBinaryTree(TreeNode* root) {
@@ -26,20 +24,14 @@ public:
     return maxDiameter;
   }
 private:
-  // return the maxDepth of the tree with root "root"
-  int maxDepth(TreeNode* root) {
-    if (!root) {    // base case, if root == nullptr
+  // input the current node, return the diameter of it
+  int maxDepth(TreeNode* curr) {
+    if (!curr) {
       return 0;
     }
-    // if we have already known the depth of root, return it
-    if (nodeToDepth.count(root)) {
-      return nodeToDepth[root];
-    }
-    // after following code, nodeToDepth[root->left], nodeToDepth[root->right] are known
-    nodeToDepth[root] = 1 + max(maxDepth(root->left), maxDepth(root->right));
-    nodeToDiameter[root] = nodeToDepth[root->left] + nodeToDepth[root->right];
-    maxDiameter = max(maxDiameter, nodeToDiameter[root]);
-    return nodeToDepth[root];
+    int left {maxDepth(curr->left)}, right {maxDepth(curr->right)};
+    maxDiameter = max(maxDiameter, left + right);
+    return max(left, right) + 1;
   }
 };
 // @leet end
