51b5c676834deafd3821ba261d476bc3dde3eee2
Ai Chang <ac@AideMacBook-Air.local>
2025-09-03 11:41:37 +0800

    543 v1.2
    Calculating the diameter of the each node when calculating the depth. I
    thought this would improve the performance greatly, but it didn't
    Performance: 20, 5

--- 543.diameter-of-binary-tree.cpp
+++ 543.diameter-of-binary-tree.cpp
@@ -14,32 +14,31 @@
 #include <algorithm>
 #include <unordered_map>
 using std::queue;
-using std::max, std::unordered_map;
+using std::max, std::unordered_map, std::max_element;
 class Solution {
 public:
   // use the map so that we can calculate the depth of each node only once
-  unordered_map<TreeNode*, int> nodeToDepth {};
-  int maxDistance {0};
+  unordered_map<TreeNode*, int> nodeToDepth {}, nodeToDiameter {};
+  int maxDiameter {0};
   // input a root node, output maxDepth(root->left) + maxDepth(root->right)
   int diameterOfBinaryTree(TreeNode* root) {
-    if (!root) {
-      return 0;
-    }
-    // I calculated the depth of the children nodes again and again
-    maxDistance = max(maxDistance, maxDepth(root->left) + maxDepth(root->right));
-    return max(maxDistance, max(diameterOfBinaryTree(root->left), diameterOfBinaryTree(root->right)));
+    maxDepth(root);
+    return maxDiameter;
   }
 private:
   // return the maxDepth of the tree with root "root"
   int maxDepth(TreeNode* root) {
+    if (!root) {    // base case, if root == nullptr
+      return 0;
+    }
     // if we have already known the depth of root, return it
     if (nodeToDepth.count(root)) {
       return nodeToDepth[root];
     }
-    if (!root) {    // base case, if root == nullptr
-      return 0;
-    }
+    // after following code, nodeToDepth[root->left], nodeToDepth[root->right] are known
     nodeToDepth[root] = 1 + max(maxDepth(root->left), maxDepth(root->right));
+    nodeToDiameter[root] = nodeToDepth[root->left] + nodeToDepth[root->right];
+    maxDiameter = max(maxDiameter, nodeToDiameter[root]);
     return nodeToDepth[root];
   }
 };
