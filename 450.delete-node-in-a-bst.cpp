f5ada53aef04c9a0b4de2e505239ecc9d2e2d384
Ai Chang <ac@AideMacBook-Air.local>
2025-09-04 20:29:19 +0800

    450 v2.0
    I learnt about this problem from labuladong, which is more concise and
    clear compared to mine
    Performance: 100, 64

--- 450.delete-node-in-a-bst.cpp
+++ 450.delete-node-in-a-bst.cpp
@@ -19,65 +19,39 @@ public:
   // third we can go up to find the minimal node in parent->right
   bool finish {false};
   TreeNode* deleteNode(TreeNode* root, int key) {
-    if (!root or (root->val == key and !root->left and !root->right)) {
+    if (!root) {
       return nullptr;
     }
-    return findNode(nullptr, root, key, false);
+    // Remember the tree is a binary search tree
+    if (root->val == key) {
+      // if root has only 0 or 1 child
+      if (!root->right) {
+        return root->left;  // maybe it's nullptr
+      }
+      else if (!root->left) {
+        return root->right;
+      }
+      // root has two children, we can find the minNode in root->right
+      TreeNode* minNode = getMin(root->right);
+      root->right = deleteNode(root->right, minNode->val);    // minNode has at most one child
+      minNode->left = root->left;
+      minNode->right = root->right;
+      root = minNode;
+    }
+    else if (root->val > key) {
+      root->left = deleteNode(root->left, key);
+    }
+    else {
+      root->right = deleteNode(root->right, key);
+    }
+    return root;
   }
 private:
-  TreeNode* findNode(TreeNode* parent, TreeNode* curr, int key, bool isLeft) {
-    if (!curr) {
-      return nullptr;
+  TreeNode* getMin(TreeNode* root) {
+    while (root->left) {
+      root = root->left;
     }
-    if (finish) {
-      return curr;
-    }
-    if (curr->val == key) {
-      deleteCurrentNode(parent, curr, isLeft);
-      return curr;
-    }
-    if (curr->left) findNode(curr, curr->left, key, true);
-    if (curr->right) findNode(curr, curr->right, key, false);
-    return curr;
-  }
-  void deleteCurrentNode(TreeNode* parent, TreeNode* curr, bool isLeft) {
-    TreeNode *left {curr->left}, *right {curr->right};
-    if (left) {
-      TreeNode *maxNode, *parentMaxNode {left};
-      // if (left->val != maxValue(curr->left))
-      if (parentMaxNode->right) {
-        while (parentMaxNode->right->right) {
-          parentMaxNode = parentMaxNode->right;
-        }
-        maxNode = parentMaxNode->right;
-        curr->val = maxNode->val;
-        parentMaxNode->right = maxNode->left;
-      }
-      else {
-        curr->val = left->val;
-        curr->left = left->left;
-      }
-    }
-    else if (right) {
-      TreeNode *minNode, *parentMinNode {right};
-      // if (left->val != maxValue(curr->left))
-      if (parentMinNode->left) {
-        while (parentMinNode->left->left) {
-          parentMinNode = parentMinNode->left;
-        }
-        minNode = parentMinNode->left;
-        curr->val = minNode->val;
-        parentMinNode->left = minNode->right;
-      }
-      else {
-        curr->val = right->val;
-        curr->right = right->right;
-      }
-    }
-    // if curr is a leaf, then just remove it from its parent
-    else {
-      isLeft ? parent->left = nullptr : parent->right = nullptr;
-    }
+    return root;
   }
 };
 // @leet end
