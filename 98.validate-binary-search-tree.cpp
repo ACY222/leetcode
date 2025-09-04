3d4b2e709b43604af6edfa6af52c8b593cea7065
Ai Chang <ac@AideMacBook-Air.local>
2025-09-04 21:00:20 +0800

    98 v1.0
    Not a difficult problem. But my initial solution will be attacked by
    INT_MIN
    Performance: 100, 75

---
+++ 98.validate-binary-search-tree.cpp
@@ -0,0 +1,32 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  bool isValidBST(TreeNode* root) {
+    return isValid(root, nullptr, nullptr);
+  }
+private:
+  bool isValid(TreeNode* root, TreeNode* min, TreeNode* max) {
+    if (!root) {
+      return true;
+    }
+    if (min and root->val <= min->val) {
+      return false;
+    }
+    if (max and root->val >= max->val) {
+      return false;
+    }
+    return isValid(root->left, min, root) and isValid(root->right, root, max);
+  }
+};
+// @leet end
