2b7416e8abaf324776e049207512f6aa9a69c68e
Ai Chang <ac@AideMacBook-Air.local>
2025-09-06 17:23:12 +0800

    rename some variables

--- 224.basic-calculator.cpp
+++ 224.basic-calculator.cpp
@@ -9,26 +9,26 @@ public:
   int calculate(string s) {
     // signs shows the sign for the sum in the brackets
     // isPositive shows the pos/neg of the last sign
-    stack<int> signs({1});
-    int total = 0, isPositive = 1, num = 0;
+    stack<int> signsForBracket({1});
+    int total = 0, currSign = 1, num = 0;
     for (char c : s) {
       if (isdigit(c)) {
         num = num * 10 + (c - '0');
       }
       else {
-        total += num * isPositive * signs.top();
+        total += num * currSign * signsForBracket.top();
         num = 0;
         switch (c) {
-          case '-': isPositive = -1; break;
-          case '+': isPositive = 1;  break;
-          case ')': signs.pop(); break;
+          case '-': currSign = -1; break;
+          case '+': currSign = 1;  break;
+          case ')': signsForBracket.pop(); break;
           case '(':
-            signs.push(signs.top() * isPositive);
-            isPositive = 1;
+            signsForBracket.push(signsForBracket.top() * currSign);
+            currSign = 1;
         }
       }
     }
-    total += num * isPositive * signs.top();
+    total += num * currSign * signsForBracket.top();
     return total;
   }
 };
