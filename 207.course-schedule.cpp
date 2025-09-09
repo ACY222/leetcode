9943c09fd94d40f40296346e51a942c5b875ef69
Ai Chang <ac@AideMacBook-Air.local>
2025-09-09 15:49:22 +0800

    207 v2.0
    Use BFS to solve it. The performance is worse than DFS. The reason might
    be that in DFS, I don't need to traverse all vertices to find the cycle,
    but I BFS, I need
    Performance: 54, 31

--- 207.course-schedule.cpp
+++ 207.course-schedule.cpp
@@ -1,56 +1,46 @@
 // @leet start
+#include <queue>
 #include <vector>
+using std::queue;
 using std::vector;
 class Solution {
 private:
-  vector<vector<int>> convertToGraph(int num, vector<vector<int>>& prerequisites) {
+  vector<vector<int>> convertToGraph(vector<vector<int>>& prerequisites, int num, vector<int>& indegrees) {
     vector<vector<int>> graph(num, vector<int>());
-    for (auto& edge : prerequisites) {
+    for (vector<int> edge : prerequisites) {
       int from = edge[1], to = edge[0];
       graph[from].push_back(to);
+      ++indegrees[to];
     }
     return graph;
   }
 
-  void traverse(int currNum, vector<vector<int>>& graph, bool& hasCycle, vector<bool>& onPath, vector<bool>& visited) {
-    if (hasCycle) {
-      return;
+  bool BFS(vector<vector<int>>& graph, vector<int>& indegrees, int num) {
+    queue<int> q;
+    for (int i = 0; i < num; ++i) {
+      if (indegrees[i] == 0) {
+        q.push(i);
+      }
     }
 
-    if (onPath[currNum]) {
-      hasCycle = true;
-      return;
+    while (!q.empty()) {
+      int parent = q.front(); q.pop();
+      --num;
+      for (int child : graph[parent]) {
+        --indegrees[child];
+        if (indegrees[child] == 0) {
+          q.push(child);
+        }
+      }
     }
-
-    if (visited[currNum]) {
-      return;
-    }
-
-    visited[currNum] = true;
-    onPath[currNum] = true;
-    for (int child : graph[currNum]) {
-      traverse(child, graph, hasCycle, onPath, visited);
-    }
-
-    onPath[currNum] = false;
+    return num == 0;
   }
 
 public:
-  // prerequisites[i] = {ai, bi}. and bi is prerequisite of ai
-  // topo sort?
   bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    if (prerequisites.size() == 0) {
-      return true;
-    }
-    // convert the prerequisites to a graph represented by adjencency list
-    vector<vector<int>> graph = convertToGraph(numCourses, prerequisites);
-    vector<bool> onPath(numCourses, false), visited(numCourses, false);
-    bool hasCycle {false};
-    for (int i = 0; i < numCourses; ++i) {
-      traverse(i, graph, hasCycle, onPath, visited);
-    }
-
-    return !hasCycle;
+    vector<int> indegrees(numCourses, 0);
+    vector<vector<int>> graph = convertToGraph(prerequisites, numCourses, indegrees);
+    return BFS(graph, indegrees, numCourses);
   }
 };
 // @leet end
