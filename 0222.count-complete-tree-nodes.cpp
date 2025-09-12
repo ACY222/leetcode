030b68106385c80c189753b58604afbd85ed279c
Ai Chang <ac@AideMacBook-Air.local>
2025-09-12 08:43:40 +0800

    rename some files

--- 0222.count-complete-tree-nodes.cpp
+++ 0222.count-complete-tree-nodes.cpp
@@ -15,25 +15,18 @@ using std::pow;
 class Solution {
 public:
   int countNodes(TreeNode* root) {
-    if (!root) {
-      return 0;
-    }
-    TreeNode *left {root}, *right {root};
-    int leftDepth {0}, rightDepth {0};
-    while (left) {
+    int depth = 0;
+    TreeNode* left {root}, *right {root};
+    while (left and right) {
       left = left->left;
-      ++leftDepth;
-    }
-    while (right) {
       right = right->right;
-      ++rightDepth;
+      ++depth;
     }
-    // if their depth are the same, it's a full binary tree
-    if (leftDepth == rightDepth) {
-      return pow(2, leftDepth) - 1;
+    if (left == right) {
+      return (int)pow(2, depth) - 1;
     }
     else {
-      return 1 + countNodes(root->left) + countNodes(root->right);
+      return countNodes(root->left) + countNodes(root->right) + 1;
     }
   }
 };
