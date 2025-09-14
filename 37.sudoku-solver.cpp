e4c2b9dac92ce53abd5b796a9f3ab74c32ce88bb
Ai Chang <ac@AideMacBook-Air.local>
2025-09-14 10:01:39 +0800

    37 v2.0
    I learnt this method from leetcode, and I can improve it later
    Performance: 47, 31

--- 37.sudoku-solver.cpp
+++ 37.sudoku-solver.cpp
@@ -5,15 +5,15 @@ class Solution {
 private:
 
   static constexpr int N = 9;
-  bool done = false;
 
-  bool isValid(const vector<vector<char>>& board, int row, int col, char num) {
+  bool isValid(const vector<vector<char>>& board,
+               const int row, const int col, const char num) {
     for (int i = 0; i < N; ++i) {
       if (board[i][col] == num or board[row][i] == num) {
         return false;
       }
     }
-    int startRow = row / 3 * 3, startCol = col / 3 * 3;
+    int startRow = (row / 3) * 3, startCol = (col / 3) * 3;
     for (int i = startRow; i < startRow + 3; ++i) {
       for (int j = startCol; j < startCol + 3; ++j) {
         if (board[i][j] == num) {
@@ -24,38 +24,28 @@ private:
     return true;
   }
 
-  bool backtrack(vector<vector<char>>& board, int row, int col) {
-    // go to another new row
-    if (col == N) {
-      col = 0;
-      ++row;
-      if (row == N) {
-        return true;
+  bool backtrack(vector<vector<char>>& board) {
+    for (int row = 0; row < N; ++row) {
+      for (int col = 0; col < N; ++col) {
+        if (board[row][col] == '.') {
+          for (char num = '1'; num <= '9'; ++num) {
+            if (isValid(board, row, col, num)) {
+              board[row][col] = num;
+              if (backtrack(board)) {
+                return true;
+              }
+              board[row][col] = '.';
+            }
+          }
+          return false; // have tries all values, but failed
+        }
       }
     }
-
-    if (board[row][col] != '.') {
-      if (backtrack(board, row, col + 1)) {
-        return true;
-      }
-      return false;
-    }
-
-    for (char num = '1'; num <= '9'; ++num) {
-      if (!isValid(board, row, col, num)) {
-        continue;
-      }
-      board[row][col] = num;
-      if (backtrack(board, row, col + 1)) {
-        return true;
-      }
-      board[row][col] = '.';
-    }
-    return false;
+    return true;
   }
 public:
   void solveSudoku(vector<vector<char>>& board) {
-    backtrack(board, 0, 0);
+    backtrack(board);
   }
 };
 // @leet end
