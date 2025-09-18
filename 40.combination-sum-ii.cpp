7158aa7d0fda2a3c36586ecdae86d6497c8df341
Ai Chang <ac@AideMacBook-Air.local>
2025-09-18 12:14:18 +0800

    40 v1.1 combination-sum-ii
    Preprocess the candidates to decrease the loop size.
    Performance: 100, 23, which is improved a lot

--- 40.combination-sum-ii.cpp
+++ 40.combination-sum-ii.cpp
@@ -1,42 +1,59 @@
 // @leet start
 #include <algorithm>
 #include <vector>
+#include <utility>
 using namespace std;
 
 class Solution {
 private:
     vector<vector<int>> res;
 
-    void backtrack(vector<int>& nums, vector<int>& path, int start, int target) {
+    void backtrack(const vector<pair<int, int>>& group, vector<int>& path, int start, int target) {
         if (target == 0) {
             res.push_back(path);
             return;
         }
 
-        for (int i = start; i < nums.size(); ++i) {
-            if (nums[i] > target) {
-                return;
+        for (int i = start; i < group.size(); ++i) {
+            int val = group[i].first;
+            int count = group[i].second;
+            if (val > target) {
+                break;
             }
+            int max_count = min(count, target / val);
+            for (int k = 1; k <= max_count; ++k) {
+                for (int l = 0; l < k; ++l) {
+                    path.push_back(val);
+                }
 
-            // This is the most important part of this problem
-            // if current num == last num, and last num is not used, then just
-            // skip. It avoids duplicated path and saves a lot of time by
-            // pruning in advance
-            if (i > start and nums[i] == nums[i - 1]) {
-                continue;
+                backtrack(group, path, i + 1, target - k * val);
+
+                for (int l = 0; l < k; ++l) {
+                    path.pop_back();
+                }
             }
-
-            path.push_back(nums[i]);
-            backtrack(nums, path, i + 1, target - nums[i]);
-            path.pop_back();
         }
     }
 public:
     // this problem is quite similar to last one
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<pair<int, int>> group;   // num and its count
         vector<int> path;
         sort(candidates.begin(), candidates.end());
-        backtrack(candidates, path, 0, target);
+        for (int i = 0; i < candidates.size(); ++i) {
+            if (candidates[i] > target) {   // the last numbers are too big
+                break;
+            }
+
+            // if we find new number
+            if (group.empty() or group.back().first != candidates[i]) {
+                group.emplace_back(candidates[i], 1);
+            }
+            else {
+                ++group.back().second;
+            }
+        }
+        backtrack(group, path, 0, target);
         return res;
     }
 };
