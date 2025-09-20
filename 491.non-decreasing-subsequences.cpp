2425c91929b3f39a6a5e4c4cc977782aecb2fdba
Ai Chang <ac@AideMacBook-Air.local>
2025-09-20 09:24:55 +0800

    491 v1.0 non-decreasing subsequences
    Similar problem with 90. But I use set to avoid repeating
    Performance: 61, 89

---
+++ 491.non-decreasing-subsequences.cpp
@@ -0,0 +1,42 @@
+// @leet start
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    set<vector<int>> res;
+    // the array should have at least two elements
+    void backtrack(const vector<int>& nums,
+                   vector<int>& path, int index) {
+        if (index == nums.size()) {
+            if (path.size() >= 2) {
+                res.insert(path);
+            }
+            return;
+        }
+        // if path is empty, or the current number is greater than the last one
+        // the case with nums[start]
+        if (path.empty() or nums[index] >= path.back()) {
+            path.push_back(nums[index]);
+            backtrack(nums, path, index + 1);
+            path.pop_back();
+        }
+        // skip the rest of group in which all the nums share the same value
+        // while (index + 1 < nums.size() and
+        //     nums[index] == nums[index + 1]) {
+        //     ++index;
+        // }
+        // the case without nums[start]
+        backtrack(nums, path,  index + 1);
+    }
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int> path;
+        backtrack(nums, path, 0);
+        return {res.begin(), res.end()};
+    }
+};
+// @leet end
