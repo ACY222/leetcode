f55bccf673e6d27b0b422a26aba5a5d0f24661b2
Ai Chang <ac@AideMacBook-Air.local>
2025-09-20 08:19:51 +0800

    review one problem

--- 1905.count-sub-islands.cpp
+++ 1905.count-sub-islands.cpp
@@ -1,47 +1,42 @@
 // @leet start
 #include <vector>
 using namespace std;
+
 class Solution {
 private:
     int m, n, count = 0;
-    static constexpr int MARK = 2;
-    const vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    const int mark = 2;
 
+    using vec = vector<vector<int>>;
     inline bool outside(int row, int col) {
         return row < 0 or row >= m or col < 0 or col >= n;
     }
-    bool dfs(const vector<vector<int>>& grid1, vector<vector<int>>& grid2,
-             int row, int col) {
-        if (grid1[row][col] != 1) {
+    const vec dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    bool dfs(const vec& grid1, vec& grid2, int r, int c) {
+        if (grid1[r][c] != 1) {
             return false;
         }
-        // we should mark the position after we know that grid1[row][col] == 1,
-        // otherwise when we arrive the other part of this island in grid2,
-        // we will mistakenly skip this position
-        grid2[row][col] = MARK;
-        bool isSub = true;
 
-        for (auto& dir : dirs) {
-            int newRow = row + dir[0], newCol = col + dir[1];
-            // if the neighbor is outside the grid
-            // or if the neighbor is not land(== 0)
-            // or has been visited(==MARK)
-            if (outside(newRow, newCol) or grid2[newRow][newCol] != 1) {
+        grid2[r][c] = mark;
+        bool isSub = true;
+        for (const auto& dir : dirs) {
+            int row = r + dir[0], col = c + dir[1];
+
+            // if the pos is out of range
+            if (outside(row, col) or grid2[row][col] != 1) {
                 continue;
             }
-            // check neighbors
-            if (!dfs(grid1, grid2, newRow, newCol)) {
+
+            if (!dfs(grid1, grid2, row, col)) {
                 isSub = false;
             }
         }
         return isSub;
     }
-
-
 public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        this->m = grid1.size();
-        this->n = grid1[0].size();
+        m = grid1.size();
+        n = grid1[0].size();
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid2[i][j] == 1) {
@@ -49,7 +44,6 @@ public:
                 }
             }
         }
-
         return count;
     }
 };
