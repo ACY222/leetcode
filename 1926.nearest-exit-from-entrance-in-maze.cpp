895620e4c8be3fe56894b370157e5710d9ea4b97
Ai Chang <ac@AideMacBook-Air.local>
2025-09-21 10:16:27 +0800

    1926 v2.0
    Use queue<pair<int, int>> instead of queue<vector<int>>, the performance
    improves greatly!
    Performance: 88, 73

--- 1926.nearest-exit-from-entrance-in-maze.cpp
+++ 1926.nearest-exit-from-entrance-in-maze.cpp
@@ -6,55 +6,40 @@ class Solution {
 private:
     int step = 0, m, n;
 
-    static constexpr char MARK = '#';
+    static constexpr char MARK = '+';
     const vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
-    inline bool atBorder(int row, int col) {
+    inline bool isBorder(int row, int col) {
         return (row == 0 or row == m - 1 or col == 0 or col == n - 1);
-    }
-    // if curr is not entrance and is on the border of the maze
-    bool isExit(vector<int>& curr, vector<int>& entrance) {
-        if (curr != entrance and atBorder(curr[0], curr[1])) {
-            return true;
-        }
-        return false;
     }
 
     inline bool outside(int row, int col) {
         return (row < 0 or row >= m or col < 0 or col >= n);
     }
 
-    void oneStepForward(queue<vector<int>>& q, vector<vector<char>>& maze,
-                        vector<int>& curr) {
-        for (auto& dir : dirs) {
-            int row = curr[0] + dir[0], col = curr[1] + dir[1];
-            // if it's not empty or it's outside, we won't move to it
-            if (outside(row, col) or maze[row][col] != '.') {
-                continue;
-            }
-            // if it's accessible, push it into the queue and mark it
-            // so that we won't visit the position again
-            q.push({row, col});
-            maze[row][col] = MARK;
-        }
-    }
-
     bool bfs(vector<vector<char>>& maze, vector<int>& entrance) {
-        queue<vector<int>> q;
-        q.push(entrance);
+        queue<pair<int, int>> q;
+        q.push({entrance[0], entrance[1]});
         maze[entrance[0]][entrance[1]] = MARK;
-        vector<int> curr;
 
         while (!q.empty()) {
-            for (int size = q.size(); size > 0; --size) {
-                curr = q.front();
-                q.pop();
-                if (isExit(curr, entrance)) {
-                    return true;
-                }
-                oneStepForward(q, maze, curr);
-            }
             ++step;
+            for (int size = q.size(); size > 0; --size) {
+                auto curr = q.front();
+                q.pop();
+
+                for (auto& dir : dirs) {
+                    int row {curr.first + dir[0]}, col {curr.second + dir[1]};
+                    if (outside(row, col) or maze[row][col] != '.') {
+                        continue;
+                    }
+                    if (isBorder(row, col)) {
+                        return true;
+                    }
+                    q.push({row, col});
+                    maze[row][col] = MARK;
+                }
+            }
         }
         return false;
     }
