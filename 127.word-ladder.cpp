09d6980ea8ebde4cc8eeefc11da172e68786a997
Ai Chang <ac@AideMacBook-Air.local>
2025-09-21 11:49:21 +0800

    127 v2.0
    Use unordered_set to store the valid words, and try to modify current
    word to get words in the set. This method is efficient because the
    words' length <= 10 and the number of possible chars is 26
    Performance: 84, 77

--- 127.word-ladder.cpp
+++ 127.word-ladder.cpp
@@ -1,81 +1,40 @@
 // @leet start
 #include <queue>
 #include <string>
+#include <utility>
+#include <unordered_set>
 #include <vector>
 using namespace std;
 
 class Solution {
-private:
-    int step = 1;
-    vector<bool> visited;
-
-    int findIndexOfEndWord(string& endWord, vector<string>& wordList) {
-        for (int i = wordList.size() - 1; i >= 0; --i) {
-            if (wordList[i] == endWord) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    bool canTransform(string& curr, string& next) {
-        int diff = 0;
-        for (int i = 0; i < curr.size(); ++i) {
-            if (curr[i] != next[i]) {
-                if (++diff >= 2) {
-                    return false;
-                }
-            }
-        }
-        return diff == 1;
-    }
-
-    // return true if we have found the endWord
-    bool pushValidWords(queue<string>& q, string& curr,
-                        int index, vector<string>& wordList) {
-        for (int i = 0; i < wordList.size(); ++i) {
-            if (visited[i]) {
-                continue;
-            }
-            if (canTransform(curr, wordList[i])) {
-                visited[i] = true;
-                if (i == index) {
-                    return true;
-                }
-                q.push(wordList[i]);
-            }
-        }
-        return false;
-    }
-
-    void bfs(string& begin, int index,  vector<string>& wordList) {
-        queue<string> q;
-        q.push(begin);
-
-        while (!q.empty()) {
-            ++step;
-            for (int size = q.size(); size > 0; --size) {
-                auto curr = q.front();
-                q.pop();
-                if (pushValidWords(q, curr, index, wordList)) {
-                    return;
-                }
-            }
-        }
-    }
-
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int index {findIndexOfEndWord(endWord, wordList)};
-        if (index == -1) {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+        if (!wordSet.count(endWord)) {
             return 0;
         }
-        visited.resize(wordList.size());
-        bfs(beginWord, index, wordList);
-        if (!visited[index]) {
-            return 0;
+
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        while (!q.empty()) {
+            auto [word, step] = q.front();
+            q.pop();
+            // if current word is endWord
+            if (word == endWord) {
+                return step;
+            }
+            for (int i = 0; i < word.size(); ++i) {
+                string temp = word;
+                for (char ch = 'a'; ch <= 'z'; ++ch) {
+                    temp[i] = ch;
+                    if (wordSet.count(temp)) {
+                        q.push({temp, step + 1});
+                        wordSet.erase(temp);
+                    }
+                }
+            }
         }
-        return step;
+        return 0;
     }
 };
 // @leet end
