9cfc8f751a26320929bd250a50ab07f653bd5aae
Ai Chang <ac@AideMacBook-Air.local>
2025-09-22 19:29:08 +0800

    354 v2.0
    This method is so enlightening! It sort the envelopes first by
    increasing width, then by decreasing height. So the 2d problem is
    changed into 1d problem, and I can use the solution of 300 directly.
    Performance: 5 6. Extremely bad

--- 354.russian-doll-envelopes.cpp
+++ 354.russian-doll-envelopes.cpp
@@ -3,21 +3,53 @@
 #include <vector>
 using namespace std;
 class Solution {
+
+    vector<int> poker_piles;
+
+    void binary_search(int poker) {
+        // [left, right], where right side is included
+        int left = 0, right = poker_piles.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (poker_piles[mid] < poker) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+
+        if (left == poker_piles.size()) {
+            poker_piles.push_back(poker);
+        }
+        else {
+            poker_piles[left] = poker;
+        }
+    }
 public:
     // both width and height are greater->can fit into
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(envelopes.begin(), envelopes.end());
-        // we can at least russian doll the envelope itself
-        vector<int> nums(envelopes.size(), 1);
-        for (int i = 1; i < envelopes.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (envelopes[i][0] > envelopes[j][0] and
-                    envelopes[i][1] > envelopes[j][1]) {
-                    nums[i] = max(nums[i], nums[j] + 1);
+        // sort the envelopes by width in increasing order
+        // if width equals, then by height in decreasing order
+        auto compare = [](vector<int> x, vector<int> y) -> bool {
+            if (x[0] < y[0]) {
+                return true;
+            }
+            else if (x[0] == y[0]) {
+                if (x[1] > y[1]) {
+                    return true;
                 }
             }
+            return false;
+        };
+        sort(envelopes.begin(), envelopes.end(), compare);
+
+        int num_piles = 0;
+        for (int i = 0; i < envelopes.size(); ++i) {
+            int poker = envelopes[i][1];
+            binary_search(poker);
         }
-        return *max_element(nums.begin(), nums.end());
+        return poker_piles.size();
     }
 };
 // @leet end
