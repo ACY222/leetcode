f7dfc4fcf88157c34ee68714baf4fb15c4d83125
Ai Chang <ac@AideMacBook-Air.local>
2025-09-23 09:46:49 +0800

    72 v1.21
    Reduce the space complexity from O(mn) to O(n)
    Performance: 60 99

--- 72.edit-distance.cpp
+++ 72.edit-distance.cpp
@@ -7,39 +7,43 @@ using namespace std;
 class Solution {
 private:
     int m, n;
-    vector<vector<int>> dp;
-
-    void init() {
-        for (int i = 1; i <= m; ++i) {
-            dp[i][0] = i;
-        }
-        for (int j = 1; j <= n; ++j) {
-            dp[0][j] = j;
-        }
-    }
 
 public:
     int minDistance(string word1, string word2) {
         this->m = word1.size();
         this->n = word2.size();
-        dp.resize(m + 1, vector<int>(n + 1));
-        init();
+        // make sure n is the shorter length
+        if (m < n) return minDistance(word2, word1);
+
+        vector<int> dp(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = i;
+        }
 
         for (int i = 1; i <= m; ++i) {
+            // prev stores the value in dp[i - 1][j - 1]
+            int prev = dp[0];
+            dp[0] = i;
             for (int j = 1; j <= n; ++j) {
+                int curr = dp[j];   // for prev in next loop
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    // dp[i][j] = dp[i - 1][j - 1]
+                    dp[j] = prev;
                 }
                 else {
-                    dp[i][j] = min({
-                        dp[i - 1][j] + 1,   // delete
-                        dp[i - 1][j - 1] + 1,// replace
-                        dp[i][j - 1] + 1    // insert
+                    dp[j] = min({
+                        // dp[i - 1][j - 1] + 1,
+                        // dp[i - 1][j] + 1,
+                        // dp[i][j - 1] + 1,
+                        prev + 1,
+                        dp[j] + 1,
+                        dp[j - 1] + 1
                     });
                 }
+                prev = curr;
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
 // @leet end
