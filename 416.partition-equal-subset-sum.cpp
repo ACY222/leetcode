49948216d0119218e60cae612292d85f63b437c2
Ai Chang <ac@AideMacBook-Air.local>
2025-09-23 11:13:52 +0800

    416 v2.0
    Really really elegant solution learnt from leetcode!
    Performance: 99 83

--- 416.partition-equal-subset-sum.cpp
+++ 416.partition-equal-subset-sum.cpp
@@ -1,44 +1,29 @@
 // @leet start
+#include <bitset>
+#include <numeric>
 #include <vector>
 using namespace std;
 class Solution {
-private:
-    int sum = 0, n;
-
-    void get_sum(const vector<int>& nums) {
-        for (const int& num : nums) {
-            sum += num;
-        }
-    }
-
-    bool dp(const vector<int>& nums, int index, int left) {
-        // if the sum of the selected part is greater than sum/2
-        if (left < 0) {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        if (sum % 2 != 0) {
             return false;
         }
-        // if we have reached the end, check if the left == 0
-        if (index == n) {
-            return left == 0;
-        }
-        // keep going forward
-        if (dp(nums, index + 1, left - nums[index]) or
-            dp(nums, index + 1, left)) {
-            return true;
+
+        // nums[i] <= 100, nums.size() <= 200
+        // so sum <= 20000, target_sum = sum / 2 <= 10000
+        // bits[i] == 1 means we can get i by adding some nums
+        // and we can always get 0
+        bitset<10001> bits(1);
+        for (auto num : nums) {
+            bits = bits | (bits << num);
+            if (bits[sum / 2]) {
+                return true;
+            }
         }
         return false;
-    }
-
-public:
-    // we have two bags, and each time we determine if we should put the
-    // current item into the bag
-    bool canPartition(vector<int>& nums) {
-        this->n = nums.size();
-        get_sum(nums);
-        // if the sum is odd, we can never partition
-        if (sum % 2 == 1) {
-            return false;
-        }
-        return dp(nums, 0, sum / 2);
     }
 };
 // @leet end
