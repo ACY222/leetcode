e9070fb03a365f7318ec17196a8d80748e1b4e05
Ai Chang <ac@AideMacBook-Air.local>
2025-09-23 12:19:43 +0800

    518 v2.0
    Use dp and compress the space
    Performance: 83 79

--- 518.coin-change-ii.cpp
+++ 518.coin-change-ii.cpp
@@ -3,27 +3,18 @@
 using namespace std;
 
 class Solution {
-private:
-    int count = 0;
-
-    void dp(vector<int>& coins, int index, int amount) {
-        if (amount == 0) {
-            ++count;
-        }
-        // if we cannot make up the amount, return false
-        if(index >= coins.size() or amount < coins[index]) {
-            return;
-        }
-
-        for (int i = 0; i <= amount / coins[index]; ++i) {
-            dp(coins, index + 1, amount - coins[index] * i);
-        }
-    }
-
 public:
-    int change(int amount, vector<int>& coins) {
-        dp(coins, 0, amount);
-        return count;
+    unsigned long long change(int amount, vector<int>& coins) {
+        vector<unsigned long long> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (const int& coin : coins) {
+            for (int j = coin; j <= amount; ++j) {
+                // dp[i][j] = dp[i - 1][j] + dp[i][j - coin]
+                dp[j] = dp[j] + dp[j - coin];
+            }
+        }
+        return dp[amount];
     }
 };
 // @leet end
