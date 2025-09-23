1f25de76e1dac439920ef698830297882c69d7e3
Ai Chang <ac@AideMacBook-Air.local>
2025-09-23 10:19:40 +0800

    583 v2.1
    Reduce the space complexity from O(mn) to O(n).
    Performance: 79 94 why the time complexity improved?

--- 583.delete-operation-for-two-strings.cpp
+++ 583.delete-operation-for-two-strings.cpp
@@ -4,41 +4,34 @@
 #include <vector>
 using namespace std;
 class Solution {
-private:
-    int m, n;
-    vector<vector<int>> dp;
-
-    void init(const string& w1, const string& w2) {
-        this->m = w1.size();
-        this->n = w2.size();
-        dp.resize(m + 1, vector<int>(n + 1));
-        for (int i = 1; i <= m; ++i) {
-            dp[i][0] = i;
-        }
-        for (int j = 1; j <= n; ++j) {
-            dp[0][j] = j;
-        }
-    }
-
 public:
     int minDistance(string word1, string word2) {
-        init(word1, word2);
+        int m = word1.size(), n = word2.size();
+        // make sure n is smaller
+        if (m < n) return minDistance(word2, word1);
+        vector<int> dp(n + 1);
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = j;
+        }
 
         for (int i = 1; i <= m; ++i) {
+            int prev = dp[0];
+            dp[0] = i;
             for (int j = 1; j <= n; ++j) {
-                // if we don't need to delete current chars
+                int curr = dp[j];
                 if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[j] = prev;
                 }
                 else {
-                    dp[i][j] = min({
-                        dp[i - 1][j] + 1,
-                        dp[i][j - 1] + 1
+                    dp[j] = min({
+                        dp[j] + 1,
+                        dp[j - 1] + 1
                     });
                 }
+                prev = curr;
             }
         }
-        return dp[m][n];
+        return dp[n];
     }
 };
 // @leet end
