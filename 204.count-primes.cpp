724ef1799df6256ea245a603e13d91b166288965
Ai Chang <ac@AideMacBook-Air.local>
2025-09-25 09:16:24 +0800

    204 v2.0
    Try a new method, but the performance is not good
    Performance: 6 66

--- 204.count-primes.cpp
+++ 204.count-primes.cpp
@@ -1,30 +1,20 @@
 // @leet start
+#include <cmath>
 #include <vector>
 using namespace std;
 class Solution {
-    bool is_prime(int x) {
-        for (int i = 2; i * i <= x; ++i) {
-            if (x % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
 public:
-    int countPrimes(int n) {
-        vector<bool> bits(n, true);
-        for (int i = 2; i * i < n; ++i) {
-            if (!is_prime(i)) {
-                continue;
-            }
-            for (int j = i * i; j < n; j += i) {
-                bits[j] = false;
-            }
-        }
+    int  countPrimes(int n) {
+        vector<bool> isPrime(n + 1, true);
         int count = 0;
         for (int i = 2; i < n; ++i) {
-            if (bits[i]) {
+            if (isPrime[i]) {
                 ++count;
+            }
+            if (i < sqrt(n)) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
         return count;
