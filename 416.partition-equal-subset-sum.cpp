5869e5c41b4409d6aa2be50462a2bd250782be22
Ai Chang <ac@AideMacBook-Air.local>
2025-09-25 08:50:27 +0800

    review two problems 72 and 416

--- 416.partition-equal-subset-sum.cpp
+++ 416.partition-equal-subset-sum.cpp
@@ -6,18 +6,14 @@ using namespace std;
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        if (sum % 2 != 0) {
+        int sum { accumulate(nums.begin(), nums.end(), 0) };
+        if (sum %  2 != 0) {
             return false;
         }
 
-        // nums[i] <= 100, nums.size() <= 200
-        // so sum <= 20000, target_sum = sum / 2 <= 10000
-        // bits[i] == 1 means we can get i by adding some nums
-        // and we can always get 0
         bitset<10001> bits(1);
         for (auto num : nums) {
+            // get the possible number we can add to
             bits = bits | (bits << num);
             if (bits[sum / 2]) {
                 return true;
--- 72.edit-distance.cpp
+++ 72.edit-distance.cpp
@@ -1,43 +1,38 @@
 // @leet start
 #include <algorithm>
+#include <numeric>
 #include <string>
 #include <vector>
 using namespace std;
-
 class Solution {
 private:
     int m, n;
+    vector<int> dp;
 
 public:
     int minDistance(string word1, string word2) {
         this->m = word1.size();
         this->n = word2.size();
-        // make sure n is the shorter length
-        if (m < n) return minDistance(word2, word1);
-
-        vector<int> dp(n + 1);
-        for (int i = 1; i <= n; ++i) {
-            dp[i] = i;
+        if (m < n) {
+            return minDistance(word2, word1);
         }
 
-        for (int i = 1; i <= m; ++i) {
-            // prev stores the value in dp[i - 1][j - 1]
-            int prev = dp[0];
+        dp.resize(n + 1);
+        iota(dp.begin(), dp.end(), 0);
+
+        for  (int i = 1; i <= m; ++i) {
+            int prev = dp[0];   // prev will be dp[i - 1][j - 1]
             dp[0] = i;
             for (int j = 1; j <= n; ++j) {
-                int curr = dp[j];   // for prev in next loop
+                int curr = dp[j];   // curr is dp[i - 1][j]
                 if (word1[i - 1] == word2[j - 1]) {
-                    // dp[i][j] = dp[i - 1][j - 1]
                     dp[j] = prev;
                 }
                 else {
                     dp[j] = min({
-                        // dp[i - 1][j - 1] + 1,
-                        // dp[i - 1][j] + 1,
-                        // dp[i][j - 1] + 1,
-                        prev + 1,
-                        dp[j] + 1,
-                        dp[j - 1] + 1
+                        prev + 1,   // replace
+                        curr + 1,   // remove
+                        dp[j-1] + 1,// insert
                     });
                 }
                 prev = curr;
