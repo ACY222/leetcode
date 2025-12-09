2ea92f45ac908b4cca8394d3b243a19f75d87097
ACY222 <838113915@qq.com>
2025-12-09 09:07:58 +0800

    review 236, 739

--- 0236.lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,30 +8,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
 class Solution {
 public:
-  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    if (!root) {
-      return nullptr;
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (!root) {
+            return nullptr;
+        }
+
+        if (root == q or root == p) {
+            return root;
+        }
+
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+        if (left and right) {
+            return root;
+        }
+        // if (!left and !right) {
+        //     return nullptr;
+        // }
+        // return left ? left : right;
+        if (left) {
+            return left;
+        }
+        return right;
     }
-    // if root is right p or q
-    if (root == p or root == q) {
-      return root;
-    }
-    // otherwise, root is neither p nor q
-    TreeNode* left { lowestCommonAncestor(root->left, p, q) };
-    TreeNode* right { lowestCommonAncestor(root->right, p, q) };
-    // case 1: if p and q are both in the tree with root "root"
-    //  then left and right are p and q
-    if (left and right) {
-      return root;
-    }
-    // case 2: if p or q are not in the tree
-    if (!left and !right) {
-      return nullptr;
-    }
-    // case 3: if only one of them is in the tree
-    return left == nullptr ? right : left;
-  }
 };
 // @leet end
--- 0739.daily-temperatures.cpp
+++ 0739.daily-temperatures.cpp
@@ -4,22 +4,22 @@
 #include <utility>
 #include <vector>
 using std::vector;
-using std::stack, std::pair;
+using std::stack;
 
 class Solution {
 public:
-  vector<int> dailyTemperatures(vector<int>& temperatures) {
-    int n {(int)temperatures.size()};
-    vector<int> res(n, 0);
-    stack<int> index {};
-    for (int i = n - 1; i >= 0; --i) {
-      while (!index.empty() and temperatures[index.top()] <= temperatures[i]) {
-        index.pop();
-      }
-      res[i] = index.empty() ? 0 : index.top() - i;
-      index.push(i);
+    vector<int> dailyTemperatures(vector<int> &temperatures) {
+        int n = temperatures.size();
+        stack<int> index;
+        vector<int> res(n, 0);
+        for (int i = n - 1; i >= 0; --i) {
+            while (!index.empty() and temperatures[index.top()] <= temperatures[i]) {
+                index.pop();
+            }
+            res[i] = index.empty() ? 0 : index.top() - i;
+            index.push(i);
+        }
+        return res;
     }
-    return res;
-  }
 };
 // @leet end
