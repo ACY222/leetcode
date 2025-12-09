4af338c51b49edf8ec974dbd54f5c35a386bff76
ACY222 <838113915@qq.com>
2025-12-09 15:41:59 +0800

    review 200 v2.0 using union find
    Performance: 57, 54. Improved a little.

--- 0200.number-of-islands.cpp
+++ 0200.number-of-islands.cpp
@@ -1,18 +1,63 @@
 #include <vector>
 #include <array>
-#include <queue>
-#include <utility>
 
 using namespace std;
 
+class UnionFind {
+public:
+    vector<int> parent;
+    int count;
+
+    // initializing the union find based on the grid
+    UnionFind(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        count = 0;
+        parent.resize(m * n, -1);
+
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                // skip if no island
+                if (grid[row][col] == '0') {
+                    continue;
+                }
+                // grid[row][col] == '1'
+                parent[row * n + col] = row * n + col;
+                ++count;
+            }
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int root_x = find(x), root_y = find(y);
+
+        if (root_x != root_y) {
+            parent[root_x] = root_y;
+            --count;
+        }
+    }
+};
+
 class Solution {
+private:
+    inline bool outside(int row, int col, int m, int n) {
+        return row < 0 or row >= m or col < 0 or col >= n;
+    }
 public:
     int numIslands(vector<vector<char>>& grid) {
         if (grid.empty()) return 0;
 
+        UnionFind uf(grid);
+
         int m = grid.size(), n = grid[0].size(), count = 0;
-        array<array<int, 2>, 4> dirs = {{
-            {0, -1}, {0, 1}, {1, 0}, {-1, 0}
+        array<array<int, 2>, 2> dirs = {{
+            {0, 1}, {1, 0}
         }};
 
         for (int row = 0; row < m; ++row) {
@@ -20,30 +65,15 @@ public:
                 if (grid[row][col] == '0') {
                     continue;
                 }
-                // if grid[row][col] == '1'
-                queue<pair<int, int>> q;
-                q.emplace(row, col);
-                ++count;
-
-                while (!q.empty()) {
-                    auto [r, c] = q.front();
-                    q.pop();
-
-                    for (auto& dir : dirs) {
-                        int new_row = r + dir[0];
-                        int new_col = c + dir[1];
-                        if (new_row < 0 or new_row >= m or new_col < 0 or new_col >= n) {
-                            continue;
-                        }
-                        if (grid[new_row][new_col] == '0') {
-                            continue;
-                        }
-                        grid[new_row][new_col] = '0';
-                        q.emplace(new_row, new_col);
+                for (auto& dir : dirs) {
+                    int new_row = row + dir[0], new_col = col + dir[1];
+                    if (new_row < m and new_col < n and grid[new_row][new_col] == '1') {
+                        uf.unite(row * n + col, new_row * n + new_col);
                     }
                 }
             }
         }
-        return count;
+
+        return uf.count;
     }
 };
