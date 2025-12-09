9bc260bfe41d2fb596a0e1462135ec6f3f965a63
ACY222 <838113915@qq.com>
2025-12-09 10:37:34 +0800

    review 200 using bfs
    Performance: 43 32. Worse than DFS

--- 0200.number-of-islands.cpp
+++ 0200.number-of-islands.cpp
@@ -1,42 +1,49 @@
-// @leet start
 #include <vector>
+#include <array>
+#include <queue>
+#include <utility>
+
 using namespace std;
+
 class Solution {
-private:
-    int m, n, count = 0;
-    const char mark = '2';
-    const vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-    inline bool outside(int row, int col) {
-        return row < 0 or row >= m or col < 0 or col >= n;
-    }
-
-    void dfs(vector<vector<char>>& grid, int row, int col) {
-        if (grid[row][col] != '1') {
-            return;
-        }
-        grid[row][col] = mark;
-        for (auto& dir : dirs) {
-            int newRow = row + dir[0], newCol = col + dir[1];
-            if (outside(newRow, newCol)) {
-                continue;
-            }
-            dfs(grid, newRow, newCol);
-        }
-    }
-
 public:
     int numIslands(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    ++count;
+        if (grid.empty()) return 0;
+
+        int m = grid.size(), n = grid[0].size(), count = 0;
+        array<array<int, 2>, 4> dirs = {{
+            {0, -1}, {0, 1}, {1, 0}, {-1, 0}
+        }};
+
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                if (grid[row][col] == '0') {
+                    continue;
+                }
+                // if grid[row][col] == '1'
+                queue<pair<int, int>> q;
+                q.emplace(row, col);
+                ++count;
+
+                while (!q.empty()) {
+                    auto [r, c] = q.front();
+                    q.pop();
+
+                    for (auto& dir : dirs) {
+                        int new_row = r + dir[0];
+                        int new_col = c + dir[1];
+                        if (new_row < 0 or new_row >= m or new_col < 0 or new_col >= n) {
+                            continue;
+                        }
+                        if (grid[new_row][new_col] == '0') {
+                            continue;
+                        }
+                        grid[new_row][new_col] = '0';
+                        q.emplace(new_row, new_col);
+                    }
                 }
             }
         }
         return count;
     }
 };
-// @leet end
