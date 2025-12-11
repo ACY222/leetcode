7ef971fa1af4cfbf634c5732d455e63afe6d40cc
ACY222 <838113915@qq.com>
2025-12-11 11:37:24 +0800

    review 92 v1.0
    Performance: 100 71

--- 0092.reverse-linked-list-ii.cpp
+++ 0092.reverse-linked-list-ii.cpp
@@ -1,37 +1,29 @@
-// @leet start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-  // input the head of the list and the range [left, right] to reverse
-  // return the head of the list with part of it being reversed
-  ListNode* reverseBetween(ListNode* head, int left, int right) {
-    if (left == 1) {    // reverse the list from head
-      return reverseN(head, right);
+    // input the head and the range [left, right]
+    // return the head of new list with part of it being reversed
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if (left == 1) {
+            return reverseToNthNode(head, right);
+        }
+
+        head->next = reverseBetween(head->next, left - 1, right - 1);
+        return head;
     }
-    head->next = reverseBetween(head->next, left - 1, right - 1);
-    return head;
-  }
 private:
-  ListNode *successor {nullptr};
-  ListNode *reverseN(ListNode *head, int n) {
-    if (n == 1) {
-      successor = head->next;
-      return head;
+    ListNode* succ {nullptr};
+
+    // input the head and n, reverse the first n nodes, return the head of new list
+    ListNode* reverseToNthNode(ListNode* head, int n) {
+        if (n == 1) {
+            succ = head->next;
+            return head;
+        }
+
+        ListNode* last = reverseToNthNode(head->next, n - 1);
+        head->next->next = head;
+        head->next = succ;
+        return last;
     }
-    ListNode *last = reverseN(head->next, n - 1);
-    head->next->next = head;
-    head->next = successor;
-    return last;
-  }
 
 };
-// @leet end
