e396595b11fbfb043dd5c7fab7035984d2f584d7
ACY222 <838113915@qq.com>
2025-12-13 20:35:58 +0800

    review 322 v1.0
    Performance: 80 68

--- 322.coin-change.cpp
+++ 322.coin-change.cpp
@@ -1,38 +1,33 @@
 // @leet start
 #include <algorithm>
 #include <vector>
+
 using namespace std;
+
 class Solution {
 private:
+    // nums[i] is the number of coins we need to make up i
     vector<int> nums;
 
-    int dp(vector<int>& coins, int amount) {
-        if (amount == 0) {
-            return 0;
-        }
-        if (amount < 0) {
-            return -1;
-        }
-        // check nums to avoid repeating calculations
-        if (nums[amount] != -1) {
-            return nums[amount];
-        }
-
-        int res = amount + 1;
-        for (const int coin : coins) {
-            int subProblem = dp(coins, amount - coin);
-            if (subProblem == -1) {
-                continue;
+    void dp(vector<int>& coins, int amount) {
+        for (int i = 1; i <= amount; ++i) {
+            for (int coin : coins) {
+                if (coin > i) {
+                    continue;
+                }
+                nums[i] = min(nums[i], nums[i - coin] + 1);
             }
-            res = min(res, 1 + subProblem);
         }
-        nums[amount] = (res == amount + 1) ? -1 : res;
-        return nums[amount];
     }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
-        nums.resize(amount + 1, -1);
-        return dp(coins, amount);
+        nums.resize(amount + 1, amount + 1);
+        nums[0] = 0;
+
+        dp(coins, amount);
+
+        return nums[amount] == amount + 1 ? -1 : nums[amount];
     }
 };
 // @leet end
