d8976149bc900fcab76e1a70ffd438c82ebf1463
ACY222 <838113915@qq.com>
2025-12-13 19:34:39 +0800

    139 v1.1
    Performance: 100 80
    According to the max word length, avoid to backtrace from the beginning

--- 139.word-break.cpp
+++ 139.word-break.cpp
@@ -1,4 +1,5 @@
 // @leet start
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <unordered_set>
@@ -10,10 +11,14 @@ public:
         unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         // dp[i]: if s[0]-s[i - 1] can be broken
         vector<bool> dp(s.size() + 1, false);
+        int maxWordLength = 0;
+        for (auto& word : wordDict) {
+            maxWordLength = max(maxWordLength, (int)word.size());
+        }
         dp[0] = true;
 
         for (int i = 1; i <= s.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
+            for (int j = i - 1; j >= 0 and (i - j) <= maxWordLength; --j) {
                 if (dp[j] == true and wordSet.count(s.substr(j, i - j))) {
                     dp[i] = true;
                     break;
