52b9679eefafacbc24b16721461bee893686b92d
ACY222 <838113915@qq.com>
2025-12-13 11:14:27 +0800

    review 141 v1.1
    Performance: 80 96
    remove an extra if statement

--- 141.linked-list-cycle.cpp
+++ 141.linked-list-cycle.cpp
@@ -7,18 +7,20 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode(int x) : val(x), next(nullptr) {}
+// };
+
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head or !head->next) {
-            return false;
-        }
-
-        ListNode *slow = head, *fast = head->next;
-
+        ListNode *fast = head, *slow = head;
         while (fast and fast->next) {
             slow = slow->next;
             fast = fast->next->next;
+
             if (slow == fast) {
                 return true;
             }
