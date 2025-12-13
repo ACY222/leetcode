b5ecf2a37dae1742ea7e2df29b3dc7afd7691dd4
ACY222 <838113915@qq.com>
2025-12-13 21:01:42 +0800

    494 v2.0
    Performance: 100 71
    Convert the problem into a 0-1 backpack problem

--- 494.target-sum.cpp
+++ 494.target-sum.cpp
@@ -1,30 +1,36 @@
 // @leet start
-#include <cmath>
+#include <numeric>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    // sum(P) + sum(N) = sum
+    // sum(P) - sum(N) = target
+    // therefore, sum(P) = (sum + target) / 2
     int findTargetSumWays(vector<int>& nums, int target) {
-        int count = 0;
-        vector<vector<int>> results(nums.size());
-        results[0].push_back(nums[0]);
-        results[0].push_back(-nums[0]);
-        for (int i = 1; i < nums.size(); ++i) {
-            results[i].reserve(pow(2, i + 1));
-            for (int sum : results[i - 1]) {
-                results[i].push_back(sum + nums[i]);
-                results[i].push_back(sum - nums[i]);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum < target or (sum + target) % 2 != 0) {
+            return 0;
+        }
+
+        // convert the problem into a 0-1 backpack problem
+        int bag_size = (sum + target) / 2;
+        if (bag_size < 0) {
+            return 0;
+        }
+
+        vector<int> dp(bag_size + 1, 0);
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int i = bag_size; i >= num; --i) {
+                dp[i] += dp[i - num];
             }
         }
 
-        for (int res : results.back()) {
-            if (res == target) {
-                ++count;
-            }
-        }
-        return count;
+        return dp[bag_size];
     }
 };
 // @leet end
