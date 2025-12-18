0d220036988d0604eb773808121d0b42be5c572e
ACY222 <838113915@qq.com>
2025-12-18 19:22:27 +0800

    240 v2.1
    Performance: 76 100
    Emmm...

--- 240.search-a-2d-matrix-ii.cpp
+++ 240.search-a-2d-matrix-ii.cpp
@@ -1,11 +1,17 @@
 // @leet start
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        static const auto io_sync_off = []() {
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            return nullptr;
+        }();
         int m = matrix.size(), n = matrix[0].size();
         int row = 0, col = n - 1;
         while (row < m and col >= 0) {
