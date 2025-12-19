a5dcc2b6d52bf6186934fc91d78883c0241521b4
ACY222 <838113915@qq.com>
2025-12-19 11:30:53 +0800

    49 v1.1
    Performance: 91 97
    Store the original strings directly, and then use std::move

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -1,6 +1,7 @@
 // @leet start
 #include <algorithm>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 #include <string>
 
@@ -9,27 +10,19 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> res;
-        unordered_map<string, vector<int>> map;     // from sorted string to indices
-        for (int i = 0; i < strs.size(); ++i) {
-            string str =  strs[i];
-            sort(str.begin(), str.end());
+        unordered_map<string, vector<string>> map;     // from sorted string to indices
 
-            auto it = map.find(str);
-            if (it == map.end()) {
-                map[str] = {i};
-            }
-            else {
-                it->second.push_back(i);
-            }
+        for (const auto& str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            map[key].push_back(str);
         }
 
+        vector<vector<string>> res;
         res.reserve(map.size());
-        for (auto& [str, indices] : map) {
-            res.push_back({});
-            for (auto index : indices) {
-                res.back().push_back(strs[index]);
-            }
+
+        for (auto& pair : map) {
+            res.push_back(std::move(pair.second));
         }
         return res;
     }
