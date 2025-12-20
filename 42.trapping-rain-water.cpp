a37ca0258c45fc609249d52f74631e7c5afd19b8
ACY222 <838113915@qq.com>
2025-12-20 15:59:59 +0800

    42 v2.0
    Performance: 100 78
    Use two pointers

--- 42.trapping-rain-water.cpp
+++ 42.trapping-rain-water.cpp
@@ -1,4 +1,5 @@
 // @leet start
+#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -6,20 +7,22 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int> max_neighbors(height.size());
-        int max_left = 0, max_right = 0, res = 0;
-        for (int i = 0; i < height.size(); ++i) {
-            max_neighbors[i] = max_left;
-            max_left = max(max_left, height[i]);
-        }
+        int left_max = 0, right_max = 0;
+        int left = 0, right = height.size() - 1;
+        int res = 0;
 
-        for (int i = height.size() - 1; i >= 0; --i) {
-            max_neighbors[i] = min(max_neighbors[i], max_right);
-            max_right = max(max_right, height[i]);
-        }
+        while (left < right) {
+            left_max = max(left_max, height[left]);
+            right_max = max(right_max, height[right]);
 
-        for (int i = 0; i < height.size(); ++i) {
-            res += max(max_neighbors[i] - height[i], 0);
+            if (left_max < right_max) {
+                res += left_max - height[left];
+                ++left;
+            }
+            else {
+                res += right_max - height[right];
+                --right;
+            }
         }
         return res;
     }
