9e2676f70772ee6eff135cce955eb2185f079c23
ACY222 <838113915@qq.com>
2025-12-20 15:49:35 +0800

    42 v1.1
    Performance: 22 43
    Use vector<int> instead of vector<pair<int, int>>

--- 42.trapping-rain-water.cpp
+++ 42.trapping-rain-water.cpp
@@ -1,5 +1,4 @@
 // @leet start
-#include <utility>
 #include <vector>
 
 using namespace std;
@@ -7,21 +6,20 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<pair<int, int>> max_neighbors(height.size());
+        vector<int> max_neighbors(height.size());
         int max_left = 0, max_right = 0, res = 0;
-
         for (int i = 0; i < height.size(); ++i) {
-            max_neighbors[i].first = max_left;
+            max_neighbors[i] = max_left;
             max_left = max(max_left, height[i]);
         }
 
         for (int i = height.size() - 1; i >= 0; --i) {
-            max_neighbors[i].second = max_right;
+            max_neighbors[i] = min(max_neighbors[i], max_right);
             max_right = max(max_right, height[i]);
         }
 
         for (int i = 0; i < height.size(); ++i) {
-            res += max(min(max_neighbors[i].first, max_neighbors[i].second) - height[i], 0);
+            res += max(max_neighbors[i] - height[i], 0);
         }
         return res;
     }
