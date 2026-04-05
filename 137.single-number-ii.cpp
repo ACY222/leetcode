17129c78f0c69d11b8e4a2a40adf130fe2428529
ACY222 <838113915@qq.com>
2026-04-05 10:35:06 +0800

    137 v2.0
    Performance: 100 86
    It seems to be a more efficient algorithm, but no performance
    improvement

--- 137.single-number-ii.cpp
+++ 137.single-number-ii.cpp
@@ -1,25 +1,19 @@
 // @leet start
-#include <array>
 #include <vector>
-
-using namespace std;
-
+using std::vector;
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        constexpr int INT_SIZE = 32;
-        int sum_by_digit = 0;
-        array<int, INT_SIZE> sum_each_digit{0};
-
-        for (int i = 0; i < INT_SIZE; ++i) {
+        int sum_by_digit{0};
+        for (int i = 0; i < 32; ++i) {
+            int sum_by_digit_i{0};
             for (auto num : nums) {
-                sum_each_digit[i] += (num & (1 << i)) >> i;
+                sum_by_digit_i += (num >> i) & 1;
             }
-            sum_each_digit[i] %= 3;
+            sum_by_digit_i %= 3;
 
-            if (sum_each_digit[i] != 0) { sum_by_digit += 1 << i; }
+            if (sum_by_digit_i != 0) { sum_by_digit |= (1 << i); }
         }
-
         return sum_by_digit;
     }
 };
