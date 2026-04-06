7e2ab99656834aa71844003ee7040f4f265ffd11
ACY222 <838113915@qq.com>
2026-04-06 11:55:44 +0800

    13 v2.0
    Performance: 18, 52
    Use a different method

--- 13.roman-to-integer.cpp
+++ 13.roman-to-integer.cpp
@@ -1,27 +1,24 @@
 // @leet start
 #include <string>
-#include <vector>
+#include <unordered_map>
 using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
         auto res{0};
-        const vector<pair<int, string>> valueSymbols{
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-            {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
-            {5, "V"},    {4, "IV"},   {1, "I"}};
+        unordered_map<char, int> symbolToValue{
+            {'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50},
+            {'X', 10},   {'V', 5},   {'I', 1}};
 
-        auto curr_idx{0};
-        for (auto [value, symbol] : valueSymbols) {
-
-            while (true) {
-                auto obj_pos{s.find(symbol, curr_idx)};
-
-                if (obj_pos == string::npos or obj_pos != curr_idx) { break; }
-                res += value;
-                curr_idx += symbol.size();
+        for (auto i{0}; i < s.size() - 1; ++i) {
+            if (symbolToValue[s[i]] < symbolToValue[s[i + 1]]) {
+                res -= symbolToValue[s[i]];
+            } else {
+                res += symbolToValue[s[i]];
             }
         }
+
+        res += symbolToValue[s.back()];
 
         return res;
     }
