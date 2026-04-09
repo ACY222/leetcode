0a58d8c6df36dd661e12d5e2b14eec369dcf7c57
ACY222 <838113915@qq.com>
2026-04-09 17:43:44 +0800

    29 v2.1
    Performance: 100 73

--- 29.divide-two-integers.cpp
+++ 29.divide-two-integers.cpp
@@ -6,9 +6,7 @@ public:
     int divide(int dividend, int divisor) {
         // avoid overflow and handle with special cases
         if (dividend == INT_MIN and divisor == -1) { return INT_MAX; }
-        if (dividend == 0) { return 0; }
-        if (divisor == 1) { return dividend; }
-        if (divisor == -1) { return -dividend; }
+        if (dividend == INT_MIN and divisor == 1) { return INT_MIN; }
 
         bool is_negative = (dividend > 0) ^ (divisor > 0);
 
@@ -17,18 +15,11 @@ public:
 
         int result = 0;
 
-        while (dividend <= divisor) {
-            int temp_divisor = divisor;
-            int count = 1;
-
-            while (divisor >= (INT_MIN >> 1)
-                   and dividend <= (temp_divisor << 1)) {
-                temp_divisor <<= 1;
-                count <<= 1;
+        for (int i = 31; i >= 0; --i) {
+            if ((dividend >> i) <= divisor and dividend <= (divisor << i)) {
+                dividend -= (divisor << i);
+                result += (1 << i);
             }
-
-            dividend -= temp_divisor;
-            result += count;
         }
 
         return is_negative ? -result : result;
