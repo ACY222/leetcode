0dbb48e50005451b416f47ac08ec5305fe5f1f54
ACY222 <838113915@qq.com>
2026-04-12 12:07:28 +0800

    41 v2.0
    Performance: 100 62

--- 41.first-missing-positive.cpp
+++ 41.first-missing-positive.cpp
@@ -1,4 +1,5 @@
 // @leet start
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -6,17 +7,16 @@ using namespace std;
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        // the first missing positive must in {1, 2, ..., size}
         int size = nums.size();
-        vector<bool> is_missed(size, true);
-
-        for (auto num : nums) {
-            if (num <= 0 or num > size) { continue; }
-            is_missed[num - 1] = false;
+        for (int i = 0; i < size; ++i) {
+            while (nums[i] > 0 and nums[i] <= size
+                   and nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
 
         for (int i = 0; i < size; ++i) {
-            if (is_missed[i]) { return i + 1; }
+            if (nums[i] != i + 1) { return i + 1; }
         }
 
         return size + 1;
