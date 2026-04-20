71f2c25e18aa05118fd5ea3a823fb8112f7b9093
ACY222 <838113915@qq.com>
2026-04-20 20:27:13 +0800

    1030 v2.0 (bucket sort, bad performance)
    Performance: 50 20

--- 1030.matrix-cells-in-distance-order.cpp
+++ 1030.matrix-cells-in-distance-order.cpp
@@ -1,45 +1,30 @@
 // @leet start
 #include <algorithm>
-#include <utility>
 #include <vector>
 using namespace std;
 class Solution {
-private:
-    inline bool is_valid(int rows, int cols, int r, int c) {
-        return r >= 0 and r < rows and c >= 0 and c < cols;
-    }
-
-    void cellsWithDist(int dist, vector<vector<int>>& cells, int rows, int cols,
-                       int r, int c) {
-        vector<pair<int, int>> groups {
-            {r, c + dist}, {r + dist, c}, {r, c - dist}, {r - dist, c}
-        };
-        static const vector<pair<int, int>> dirs {
-            {1, -1}, {-1, -1}, {-1, 1}, {1, 1}
-        };
-
-        for (int i = 0; i < dist; ++i) {
-            for (int idx = 0; idx < 4; ++idx) {
-                int row = groups[idx].first + dirs[idx].first * i;
-                int col = groups[idx].second + dirs[idx].second * i;
-                if (!is_valid(rows, cols, row, col)) { continue; }
-                cells.push_back({row, col});
-            }
-        }
-    }
-
 public:
     vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter,
                                           int cCenter) {
+        int max_dist =
+            max(rCenter, rows - 1 - rCenter) + max(cCenter, cols - 1 - cCenter);
+
+        vector<vector<vector<int>>> buckets(max_dist + 1);
+
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                int dist = abs(r - rCenter) + abs(c - cCenter);
+                buckets[dist].push_back({r, c});
+            }
+        }
+
         vector<vector<int>> cells;
         cells.reserve(rows * cols);
-        cells.push_back({rCenter, cCenter});
 
-        int max_dist = max(max(rCenter + cCenter, rows - 1 - rCenter + cCenter),
-                           max(rCenter + cols - 1 - cCenter,
-                               rows - 1 - rCenter + cols - 1 - cCenter));
-        for (int dist = 1; dist <= max_dist; ++dist) {
-            cellsWithDist(dist, cells, rows, cols, rCenter, cCenter);
+        for (int dist = 0; dist <= max_dist; ++dist) {
+            for (auto& cell : buckets[dist]) {
+                cells.push_back({cell});
+            }
         }
         return cells;
     }
