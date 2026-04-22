e6b9c27a95ec07d70be81b7dab63bc3a0b5aac28
ACY222 <838113915@qq.com>
2026-04-22 17:04:56 +0800

    946 v1.0
    Performance: 100 45

---
+++ 946.validate-stack-sequences.cpp
@@ -0,0 +1,31 @@
+// @leet start
+#include <stack>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int i = 0, j = 0;
+
+        while (i < pushed.size()) {
+            if (!st.empty() and st.top() == popped[j]) {
+                st.pop();
+                ++j;
+            } else {
+                st.push(pushed[i]);
+                ++i;
+            }
+        }
+
+        while (j < popped.size()) {
+            if (st.top() != popped[j]) { return false; }
+
+            st.pop();
+            ++j;
+        }
+
+        return true;
+    }
+};
+// @leet end
