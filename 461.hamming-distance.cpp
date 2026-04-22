00a6223f3e6d91dc6a0f0d18efabcfd203ed20e8
ACY222 <838113915@qq.com>
2026-04-22 08:43:20 +0800

    461 v1.1 (hamming distance between two numbers)
    Performance: 100 37

--- 461.hamming-distance.cpp
+++ 461.hamming-distance.cpp
@@ -4,10 +4,7 @@ public:
     int hammingDistance(int x, int y) {
         int dist = 0;
         while (x > 0 or y > 0) {
-            int last_x = x % 2;
-            int last_y = y % 2;
-
-            if (last_x != last_y) { ++dist; }
+            if ((x & 1) != (y & 1)) { ++dist; }
 
             x = x >> 1;
             y = y >> 1;
