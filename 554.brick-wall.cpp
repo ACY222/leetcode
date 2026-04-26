7b11d423db3caaa10c4fb0af1084725e2608194d
ACY222 <838113915@qq.com>
2026-04-26 16:11:07 +0800

    554 v2.0 (use unordered_map instead of vector, int64_t instead of int)
    Performance: 85 55
    Space complexity changes from O(W) to O(N) (the number of bricks)

--- 554.brick-wall.cpp
+++ 554.brick-wall.cpp
@@ -1,34 +1,26 @@
 // @leet start
 #include <algorithm>
+#include <cstdint>
+#include <unordered_map>
 #include <vector>
-using std::vector;
+using namespace std;
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        int width = 0, depth = wall.size();
-        for (auto brick : wall[0]) {
-            width += brick;
-        }
+        unordered_map<int64_t, int> edge_counts;
+        int max_edges = 0;
 
-        if (width == 1) { return depth; }
-        vector<int> num_edges(width - 1);
+        for (const auto& row : wall) {
+            int64_t curr_width = 0;
+            for (int i = 0; i < row.size() - 1; ++i) {
+                curr_width += row[i];
+                edge_counts[curr_width]++;
 
-        for (int i = 0; i < depth; ++i) {
-            int curr = -1;
-            for (int j = 0; j < wall[i].size() - 1; ++j) {
-                curr += wall[i][j];
-                num_edges[curr]++;
+                max_edges = max(max_edges, edge_counts[curr_width]);
             }
         }
 
-        return depth - *std::max_element(num_edges.begin(), num_edges.end());
+        return wall.size() - max_edges;
     }
 };
 // @leet end
-// int main() {
-//     // vector<vector<int>> wall {{1, 2, 2, 1}, {3, 1, 2}, {1, 3, 2},
-//     //                           {2, 4},       {3, 1, 2}, {1, 3, 1, 1}};
-//     vector<vector<int>> wall {{1}, {1}, {1}};
-//     Solution sol;
-//     std::cout << sol.leastBricks(wall) << '\n';
-// }
