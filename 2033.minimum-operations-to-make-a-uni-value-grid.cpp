48a2fbb26506065268f2e143a16fc061c62cd17f
ACY222 <838113915@qq.com>
2026-04-28 10:58:05 +0800

    2033 v2.0 (quick select, no need to sort)
    Performance: 85 88

--- 2033.minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033.minimum-operations-to-make-a-uni-value-grid.cpp
@@ -7,18 +7,23 @@ class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
         int m = grid.size(), n = grid[0].size();
-        vector<int> sorted(m * n);
+        int mod = grid[0][0] % x;
+
+        vector<int> nums;
+        nums.reserve(m * n);
+
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                sorted[i * n + j] = grid[i][j];
-                if ((grid[i][j] - grid[0][0]) % x != 0) { return -1; }
+                if (grid[i][j] % x != mod) { return -1; }
+                nums.push_back(grid[i][j]);
             }
         }
 
-        sort(sorted.begin(), sorted.end());
         int mid_idx = m * n / 2, num_operations = 0;
+        nth_element(nums.begin(), nums.begin() + mid_idx, nums.end());
+        int median = nums[mid_idx];
         for (int i = 0; i < m * n; ++i) {
-            num_operations += abs(sorted[i] - sorted[mid_idx]);
+            num_operations += abs(nums[i] - median);
         }
 
         return num_operations / x;
