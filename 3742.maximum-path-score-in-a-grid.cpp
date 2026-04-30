165c94f185a2200b9773369303375e0960191d34
ACY222 <838113915@qq.com>
2026-04-30 12:49:56 +0800

    3742 v2.0 (use vector<vector<int>>, with capacity min(m + n, k) + rolling array)
    Performance: 88 83

--- 3742.maximum-path-score-in-a-grid.cpp
+++ 3742.maximum-path-score-in-a-grid.cpp
@@ -1,79 +1,64 @@
 // @leet start
 #include <algorithm>
-#include <map>
+#include <utility>
 #include <vector>
 using namespace std;
 
 class Solution {
-private:
-    void initialize(const vector<vector<int>>& grid,
-                    vector<vector<map<int, int>>>& dp, int k) {
-        dp[0][0][0] = 0;
-
-        for (int y = 1; y < grid[0].size(); ++y) {
-            int curr_score = grid[0][y], curr_cost = (grid[0][y] == 0) ? 0 : 1;
-
-            auto [last_cost, last_score] = *dp[0][y - 1].begin();
-            int score = last_score + curr_score;
-            int cost = last_cost + curr_cost;
-
-            if (cost > k) { break; }
-            dp[0][y][cost] = score;
-        }
-
-        for (int x = 1; x < grid.size(); ++x) {
-            int curr_score = grid[x][0], curr_cost = (grid[x][0] == 0) ? 0 : 1;
-
-            auto [last_cost, last_score] = *dp[x - 1][0].begin();
-            int score = last_score + curr_score;
-            int cost = last_cost + curr_cost;
-
-            if (cost > k) { break; }
-            dp[x][0][cost] = score;
-        }
-    }
-
 public:
     int maxPathScore(vector<vector<int>>& grid, int k) {
         int m = grid.size(), n = grid[0].size();
-        // dp[x][y][c] = the max score at (x, y) with cost c
-        vector<vector<map<int, int>>> dp(m, vector<map<int, int>>(n));
+        k = min(k, m + n - 1);
+        // dp[j][c] = max score in cell (curr_row, col j) with exact cost c
+        // initialized with -1 means irreachable
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
 
-        initialize(grid, dp, k);
+        dp[0][0] = 0;
+        for (int j = 1; j < n; ++j) {
+            int val = grid[0][j];
+            int cost = (val == 0) ? 0 : 1, score = val;
 
-        for (int x = 1; x < m; ++x) {
-            for (int y = 1; y < n; ++y) {
-                int val = grid[x][y];
-                int curr_score = val, curr_cost = (val == 0) ? 0 : 1;
+            for (int last_cost = 0; last_cost + cost <= k; ++last_cost) {
+                int last_score = dp[j - 1][last_cost];
 
-                for (const auto [last_cost, last_score] : dp[x][y - 1]) {
-                    int cost = last_cost + curr_cost,
-                        score = last_score + curr_score;
-
-                    // prune
-                    if (cost > k) { break; }
-                    dp[x][y][cost] = score;
-                }
-
-                for (const auto [last_cost, last_score] : dp[x - 1][y]) {
-                    int cost = last_cost + curr_cost,
-                        score = last_score + curr_score;
-                    if (cost > k) { break; }
-
-                    // if not found or less than score, update it
-                    if (dp[x][y].find(cost) == dp[x][y].end()
-                        or dp[x][y][cost] < score) {
-                        dp[x][y][cost] = score;
-                    }
+                if (last_score != -1) {
+                    dp[j][last_cost + cost] = last_score + score;
                 }
             }
         }
 
-        int max_score = -1;
-        for (auto [cost, score] : dp[m - 1][n - 1]) {
-            max_score = max(score, max_score);
+        for (int i = 1; i < m; ++i) {
+            vector<vector<int>> next_dp(n, vector<int>(k + 1, -1));
+
+            for (int j = 0; j < n; ++j) {
+                int val = grid[i][j];
+                int cost = (val == 0) ? 0 : 1, score = val;
+
+                for (int last_cost = 0; last_cost + cost <= k; ++last_cost) {
+                    int max_last_score = -1;
+                    // move from above cell
+                    if (dp[j][last_cost] != -1) {
+                        max_last_score = dp[j][last_cost];
+                    }
+
+                    // move from left cell
+                    if (j > 0 and next_dp[j - 1][last_cost] != -1) {
+                        max_last_score =
+                            max(next_dp[j - 1][last_cost], max_last_score);
+                    }
+
+                    if (max_last_score != -1) {
+                        next_dp[j][last_cost + cost] = max_last_score + score;
+                    }
+                }
+            }
+            dp = std::move(next_dp);
         }
 
+        int max_score = -1;
+        for (auto score : dp[n - 1]) {
+            max_score = max(score, max_score);
+        }
         return max_score;
     }
 };
