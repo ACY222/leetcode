e3df215408293cd2dc7fe5793eff405acc4f57ec
ACY222 <838113915@qq.com>
2026-05-01 10:57:34 +0800

    396 v1.1 (iteration)
    Performance: 9 97

--- 396.rotate-function.cpp
+++ 396.rotate-function.cpp
@@ -5,14 +5,21 @@ class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
         int rotate_func = INT_MIN, n = nums.size();
+        int sum = 0, rotate_func_last = 0;
 
-        for (int k = 0; k < n; ++k) {
-            int rotate_func_k = 0;
-            for (int i = 0; i < n; ++i) {
-                rotate_func_k += i * nums[(i - k + n) % n];
-            }
+        for (int i = 0; i < n; ++i) {
+            rotate_func_last += (i * nums[i]);
+            sum += nums[i];
+        }
+
+        rotate_func = rotate_func_last;
+
+        for (int k = 1; k < n; ++k) {
+            int rotate_func_k =
+                rotate_func_last + sum - n * nums[(2 * n - k) % n];
 
             rotate_func = max(rotate_func, rotate_func_k);
+            rotate_func_last = rotate_func_k;
         }
         return rotate_func;
     }
