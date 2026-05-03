b19e9e53b26a7b4929cdace11a0f682e03a6a505
ACY222 <838113915@qq.com>
2026-05-03 10:46:54 +0800

    796 v2.0 (check if (s + s) contains goal
    Performance: 100 62

--- 796.rotate-string.cpp
+++ 796.rotate-string.cpp
@@ -6,15 +6,8 @@ public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) { return false; }
 
-        int n = s.size();
-        for (int offset = 0; offset < n; ++offset) {
-            int i = 0;
-            for (; i < n; ++i) {
-                if (s[(offset + i) % n] != goal[i]) { break; }
-            }
-
-            if (i == n) { return true; }
-        }
+        s += s;
+        if (s.find(goal, 0) != string::npos) { return true; }
 
         return false;
     }
