6a34e757c3a05e0461a6a0f6cd51acaf5c0f7877
ACY222 <838113915@qq.com>
2026-05-07 09:34:31 +0800

    3660 v2.0 (UF, skip united num, prefix max + suffix min to skip cut) (one step forward)

--- 3660.jump-game-ix.cpp
+++ 3660.jump-game-ix.cpp
@@ -1,4 +1,5 @@
 // @leet start
+#include <algorithm>
 #include <numeric>
 #include <vector>
 using namespace std;
@@ -32,10 +33,23 @@ private:
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
+        vector<int> pre_max(n), suf_min(n);
+        pre_max[0] = nums[0];
+        suf_min[n - 1] = nums[n - 1];
+
+        for (int i = 1; i < n - 1; ++i) {
+            pre_max[i] = max(nums[i], pre_max[i - 1]);
+            suf_min[n - i - 1] = min(suf_min[n - i], nums[n - i - 1]);
+        }
+
         UnionFind uf(n);
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n - 1; ++i) {
+            // a cut occurs!
+            if (pre_max[i] < suf_min[i + 1]) { continue; }
+            // skip united num
             if (uf.find(i) != i) { continue; }
+
             for (int j = i + 1; j < n; ++j) {
                 if (nums[i] > nums[j]) { uf.unite(nums, i, j); }
             }
