2936b29a1d89ef10ae5b4d5dcf99d1c9d53f94c6
ACY222 <838113915@qq.com>
2026-05-07 10:26:32 +0800

    3660 v3.0 (prefix max + suffix min)
    Performance: 41 66

--- 3660.jump-game-ix.cpp
+++ 3660.jump-game-ix.cpp
@@ -1,64 +1,35 @@
 // @leet start
 #include <algorithm>
-#include <numeric>
 #include <vector>
 using namespace std;
 class Solution {
-private:
-    struct UnionFind {
-        vector<int> parent;
-
-        UnionFind(int n) : parent(n) { iota(parent.begin(), parent.end(), 0); }
-
-        int find(int x) {
-            if (x == parent[x]) { return x; }
-
-            return (parent[x] = find(parent[x]));
-        }
-
-        void unite(const vector<int>& nums, int x, int y) {
-            int root_x = find(x);
-            int root_y = find(y);
-
-            if (root_x != root_y) {
-                if (nums[root_x] >= nums[root_y]) {
-                    parent[root_y] = root_x;
-                } else {
-                    parent[root_x] = root_y;
-                }
-            }
-        }
-    };
-
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
         vector<int> pre_max(n), suf_min(n);
+
+        // initialize pre_max and suf_min
         pre_max[0] = nums[0];
         suf_min[n - 1] = nums[n - 1];
-
-        for (int i = 1; i < n - 1; ++i) {
+        for (int i = 1; i < n; ++i) {
             pre_max[i] = max(nums[i], pre_max[i - 1]);
             suf_min[n - i - 1] = min(suf_min[n - i], nums[n - i - 1]);
         }
 
-        UnionFind uf(n);
+        vector<int> result(n);
 
-        for (int i = 0; i < n - 1; ++i) {
-            // a cut occurs!
-            if (pre_max[i] < suf_min[i + 1]) { continue; }
-            // skip united num
-            if (uf.find(i) != i) { continue; }
-
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] > nums[j]) { uf.unite(nums, i, j); }
+        // suppose we're checking the last number j
+        // if nums[j] == pre_max[j], then it's obvious
+        // if nums[j] < pre_max[j], assume pre_max[j] == nums[i], where i < j
+        // then we know that i < j and nums[i] > nums[j], so we can jump from i
+        // to j, so result[j] = pre_max[j]
+        result[n - 1] = pre_max[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            if (pre_max[i] > suf_min[i + 1]) {
+                result[i] = result[i + 1];
+            } else {
+                result[i] = pre_max[i];
             }
-        }
-
-        vector<int> result;
-        result.reserve(n);
-        for (int i = 0; i < n; ++i) {
-            result.push_back(nums[uf.find(i)]);
         }
 
         return result;
