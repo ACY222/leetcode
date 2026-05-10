e1b289de493b050c04fdaf9f034154b5f9b22831
ACY222 <838113915@qq.com>
2026-05-10 23:31:03 +0800

    2770 v2.0 (DP)
    Performance: 81 69

--- 2770.maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770.maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,35 +1,25 @@
 // @leet start
-#include <unordered_set>
-#include <utility>
 #include <vector>
 using namespace std;
 class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size(), max_jumps = -1, count = 1;
-        unordered_set<int> from;
+        vector<int> dp(n, -1);
+        dp[0] = 0;
 
-        from.insert(0);
+        for (int i = 0; i < n - 1; ++i) {
+            // this index is irreacheable
+            if (dp[i] == -1) { continue; }
 
-        while (!from.empty()) {
-            unordered_set<int> to;
-            for (int i : from) {
-                for (int j = i + 1; j < n; ++j) {
-                    if (to.find(j) == to.end()
-                        and abs(nums[j] - nums[i]) <= target) {
-                        if (j == n - 1) { max_jumps = count; }
-
-                        to.insert(j);
-                    }
+            for (int j = i + 1; j < n; ++j) {
+                if (abs(nums[i] - nums[j]) <= target and dp[i] >= dp[j]) {
+                    dp[j] = dp[i] + 1;
                 }
             }
-
-            from = std::move(to);
-            ++count;
         }
-        return max_jumps;
+
+        return dp.back();
     }
 };
 // @leet end
-//
-// 1 2 1; 0
