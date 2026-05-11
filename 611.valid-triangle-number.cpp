33f46f07fca8452529808677601b79a5aa25cfb9
ACY222 <838113915@qq.com>
2026-05-11 13:45:56 +0800

    611 v2.0 (double pointers)
    Performance: 89 48

--- 611.valid-triangle-number.cpp
+++ 611.valid-triangle-number.cpp
@@ -8,17 +8,14 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size(), count = 0;
-        for (int i = 0; i < n - 2; ++i) {
-            int last_idx = 0;
-            for (int j = i + 1; j < n - 1; ++j) {
-                for (int k = max(j + 1, last_idx); k < n; ++k) {
-                    if (nums[i] + nums[j] <= nums[k]) {
-                        count += (k - j - 1);
-                        last_idx = k;
-                        break;
-                    } else if (k == n - 1) {
-                        count += (k - j);
-                    }
+        for (int k = n - 1; k >= 2; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    --j;
+                } else {
+                    ++i;
                 }
             }
         }
