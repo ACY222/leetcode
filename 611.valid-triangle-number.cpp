d4b038057ecc3bf54bd79cc7804cad2c7176f6a8
ACY222 <838113915@qq.com>
2026-05-11 13:41:54 +0800

    611 v1.1 (make some small improvements)
    Performance: 9 12

--- 611.valid-triangle-number.cpp
+++ 611.valid-triangle-number.cpp
@@ -9,9 +9,16 @@ public:
 
         int n = nums.size(), count = 0;
         for (int i = 0; i < n - 2; ++i) {
+            int last_idx = 0;
             for (int j = i + 1; j < n - 1; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    if (nums[i] + nums[j] > nums[k]) { ++count; }
+                for (int k = max(j + 1, last_idx); k < n; ++k) {
+                    if (nums[i] + nums[j] <= nums[k]) {
+                        count += (k - j - 1);
+                        last_idx = k;
+                        break;
+                    } else if (k == n - 1) {
+                        count += (k - j);
+                    }
                 }
             }
         }
