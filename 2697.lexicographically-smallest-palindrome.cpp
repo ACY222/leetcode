699f33155187d0a7d8d513222515c78c76868901
ACY222 <838113915@qq.com>
2026-05-16 09:16:57 +0800

    2697 v1.1 (remove the redundant count)
    Performance: 77 55

--- 2697.lexicographically-smallest-palindrome.cpp
+++ 2697.lexicographically-smallest-palindrome.cpp
@@ -5,14 +5,11 @@ class Solution {
 public:
     string makeSmallestPalindrome(string s) {
         int n = s.size();
-        int count = 0;
         for (int i = 0; i < n / 2; ++i) {
             if (s[i] < s[n - 1 - i]) {
                 s[n - 1 - i] = s[i];
-                ++count;
             } else if (s[i] > s[n - 1 - i]) {
                 s[i] = s[n - 1 - i];
-                ++count;
             }
         }
         return s;
