0ffc65fd2377e4f1dbf5fda53c63b5efcc8be448
ACY222 <838113915@qq.com>
2026-05-16 09:20:15 +0800

    2697 v1.2 (use 'left', 'right' instead of 'i')
    Performance: 6 55

--- 2697.lexicographically-smallest-palindrome.cpp
+++ 2697.lexicographically-smallest-palindrome.cpp
@@ -1,16 +1,17 @@
 // @leet start
+#include <algorithm>
 #include <string>
 using namespace std;
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-        int n = s.size();
-        for (int i = 0; i < n / 2; ++i) {
-            if (s[i] < s[n - 1 - i]) {
-                s[n - 1 - i] = s[i];
-            } else if (s[i] > s[n - 1 - i]) {
-                s[i] = s[n - 1 - i];
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                s[left] = s[right] = min(s[left], s[right]);
             }
+            --right;
+            ++left;
         }
         return s;
     }
