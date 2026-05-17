2eb7c7c8e5c877902de56d9cdc4f5850b5a11ac4
ACY222 <838113915@qq.com>
2026-05-17 21:41:15 +0800

    1796 v1.1 (remove a redundant if statement)
    Performance: 100 99

--- 1796.second-largest-digit-in-a-string.cpp
+++ 1796.second-largest-digit-in-a-string.cpp
@@ -9,20 +9,14 @@ public:
             if (ch < '0' or ch > '9') { continue; }
             int digit = ch - '0';
 
-            if (first == -1) {
-                first = digit;
+            // first, digit, second
+            if (first > digit and digit > second) {
+                second = digit;
             }
-            // first != -1, second might be -1
-            else {
-                // first, digit, second
-                if (first > digit and digit > second) {
-                    second = digit;
-                }
-                // digit, first, second
-                else if (digit > first) {
-                    second = first;
-                    first = digit;
-                }
+            // digit, first, second
+            else if (digit > first) {
+                second = first;
+                first = digit;
             }
         }
 
