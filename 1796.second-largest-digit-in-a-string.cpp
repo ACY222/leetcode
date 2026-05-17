b30f40d6c9abd8c6c767159bff8b166aadbaf435
ACY222 <838113915@qq.com>
2026-05-17 21:39:28 +0800

    1796 v1.0 (simple idea)
    Performance: 11 90

---
+++ 1796.second-largest-digit-in-a-string.cpp
@@ -0,0 +1,32 @@
+// @leet start
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int secondHighest(string s) {
+        int first = -1, second = -1;
+        for (char ch : s) {
+            if (ch < '0' or ch > '9') { continue; }
+            int digit = ch - '0';
+
+            if (first == -1) {
+                first = digit;
+            }
+            // first != -1, second might be -1
+            else {
+                // first, digit, second
+                if (first > digit and digit > second) {
+                    second = digit;
+                }
+                // digit, first, second
+                else if (digit > first) {
+                    second = first;
+                    first = digit;
+                }
+            }
+        }
+
+        return second;
+    }
+};
+// @leet end
