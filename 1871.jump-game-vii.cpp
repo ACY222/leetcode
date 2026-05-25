94702f5be9096a63344f8245473fc1745e44e1b0
ACY222 <838113915@qq.com>
2026-05-25 23:50:52 +0800

    1871 v2.0 (DP)
    Performance: 54 96

--- 1871.jump-game-vii.cpp
+++ 1871.jump-game-vii.cpp
@@ -1,65 +1,28 @@
 // @leet start
-#include <algorithm>
-#include <stack>
 #include <string>
+#include <vector>
 using namespace std;
 class Solution {
 public:
     // i -> j: i + minJump <= j <= min(i + maxJump, s.size - 1) and s[j] == '0'
-    // if I can reach j, I must come from i, where j - maxJump <= i <= j -
-    // minJump
     bool canReach(string s, int minJump, int maxJump) {
         int n = s.size();
 
-        if (s.back() == '1') { return false; }
+        if (s[n - 1] == '1') { return false; }
 
-        stack<int> forward {{0}};
-        stack<int> backward {{n - 1}};
+        vector<bool> dp(n, false);
+        dp[0] = true;
 
-        vector<bool> left_visited(n, false), right_visited(n, false);
+        int active = 0; // the count of reachable points in the window
+        for (int i = 1; i < n; ++i) {
+            if (i >= minJump and dp[i - minJump]) { ++active; }
 
-        left_visited[0] = true, right_visited[n - 1] = true;
+            if (i > maxJump and dp[i - maxJump - 1]) { --active; }
 
-        while (!forward.empty() and !backward.empty()) {
-            int left = forward.top();
-            forward.pop();
-
-            int right = backward.top();
-            backward.pop();
-
-            if (left_visited[right] == true or right_visited[left] == true) {
-                return true;
-            }
-
-            for (int j = left + minJump; j <= min(left + maxJump, n - 1); ++j) {
-                if (left_visited[j] == false and s[j] == '0') {
-                    if (right_visited[j] == true) { return true; }
-
-                    forward.push(j);
-                    left_visited[j] = true;
-                }
-            }
-
-            for (int i = right - minJump; i >= max(right - maxJump, 0); --i) {
-                if (right_visited[i] == false and s[i] == '0') {
-                    if (left_visited[i] == true) { return true; }
-
-                    backward.push(i);
-                    right_visited[i] = true;
-                }
-            }
+            if (s[i] == '0' and active > 0) { dp[i] = true; }
         }
 
-        return false;
+        return dp[n - 1];
     }
 };
 // @leet end
-//
-// 0 1 2 3 4 5
-// 0 1 1 0 1 0
-//
-// 0 (2, 3)
-// 3 (5, 6)
-// 5
-//
-// 5 (2, 3)
