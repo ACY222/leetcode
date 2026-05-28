0fc0d7bc5fb9af6c536dfe496dc3e31c6e0544a9
ACY222 <838113915@qq.com>
2026-05-28 23:15:53 +0800

    3093 v2.0 (Trie)
    Performance: 32 36

--- 3093.longest-common-suffix-queries.cpp
+++ 3093.longest-common-suffix-queries.cpp
@@ -1,66 +1,84 @@
 // @leet start
-// #include <format>
-// #include <iostream>
+#include <algorithm>
+#include <array>
+#include <memory>
+#include <string>
 #include <vector>
-
 using namespace std;
-
 class Solution {
 private:
-    int get_len_common_suffix(const string& word1, const string& word2) {
-        int len = 0, i = word1.size() - 1, j = word2.size() - 1;
-        while (i >= 0 and j >= 0 and word1[i] == word2[j]) {
-            ++len;
-            --i;
-            --j;
-        }
+    class Trie {
+    private:
+        struct TrieNode {
+            array<unique_ptr<TrieNode>, 26> children {};
+            int best_idx;
 
-        return len;
-    }
+            TrieNode(int x) : best_idx(x) {}
+        };
 
-public:
-    // longest common suffix
-    // 1. smaller, 2. earlier
-    vector<int> stringIndices(vector<string>& wordsContainer,
-                              vector<string>& wordsQuery) {
-        int n = wordsQuery.size();
-        vector<int> indices;
-        indices.reserve(n);
+        unique_ptr<TrieNode> root;
 
-        for (const string& query : wordsQuery) {
-            int best_idx = 0;
-            int max_len = get_len_common_suffix(query, wordsContainer[0]);
+        int search(const string& query) {
+            auto curr = root.get();
+            for (int i = query.size() - 1; i >= 0; --i) {
+                int idx = query[i] - 'a';
 
-            for (int idx = 1; idx < wordsContainer.size(); ++idx) {
-                const auto& word = wordsContainer[idx];
+                if (!curr->children[idx]) { return curr->best_idx; }
 
-                int curr_len = get_len_common_suffix(query, word);
-
-                if (curr_len > max_len
-                    or (curr_len == max_len
-                        and word.size() < wordsContainer[best_idx].size())) {
-                    // cout << format("max_len = {}, max_idx = {}, curr_len
-                    // =
-                    // {}, "
-                    //                "curr_idx = {}\n",
-                    //                max_len_common, best_idx,
-                    //                curr_len_common, idx);
-                    best_idx = idx;
-                    max_len = curr_len;
-                }
+                curr = curr->children[idx].get();
             }
 
-            indices.push_back(best_idx);
+            return curr->best_idx;
         }
 
-        return indices;
+    public:
+        Trie() : root(make_unique<TrieNode>(0)) {}
+
+        void build(const vector<string>& words) {
+            for (int index = 0; index < words.size(); ++index) {
+                const auto& word = words[index];
+
+                auto curr = root.get();
+                if (word.size() < words[curr->best_idx].size()) {
+                    curr->best_idx = index;
+                }
+                for (int i = word.size() - 1; i >= 0; --i) {
+                    int idx = word[i] - 'a';
+
+                    if (!curr->children[idx]) {
+                        curr->children[idx] = make_unique<TrieNode>(index);
+                    } else {
+                        // if word.size is smaller, update index
+                        if (word.size()
+                            < words[curr->children[idx]->best_idx].size()) {
+                            curr->children[idx]->best_idx = index;
+                        }
+                    }
+
+                    curr = curr->children[idx].get();
+                }
+            }
+        }
+
+        vector<int> search_all(const vector<string>& queries) {
+            vector<int> indices;
+            indices.reserve(queries.size());
+
+            for_each(queries.begin(), queries.end(),
+                     [&indices, this](auto& query) {
+                         indices.push_back(search(query));
+                     });
+
+            return indices;
+        }
+    };
+
+public:
+    vector<int> stringIndices(vector<string>& wordsContainer,
+                              vector<string>& wordsQuery) {
+        Trie trie;
+        trie.build(wordsContainer);
+        return trie.search_all(wordsQuery);
     }
 };
 // @leet end
-// int main() {
-//     Solution sol;
-//     vector<string> wordsContainer {"abcdefgh", "poiuygh", "ghghgh"},
-//         wordsQuery {"gh", "acbfgh", "acbfegh"};
-//
-//     sol.stringIndices(wordsContainer, wordsQuery);
-// }
