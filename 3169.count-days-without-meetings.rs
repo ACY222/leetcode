e5c6c8b48b550cefba8463be854ee45943f95298
ACY222 <838113915@qq.com>
2026-07-10 00:01:39 +0800

    3169 v1.1 sort and scan
    Perf: 50 100

--- 3169.count-days-without-meetings.rs
+++ 3169.count-days-without-meetings.rs
@@ -1,24 +1,38 @@
 // @leet start
 impl Solution {
-    pub fn count_days(days: i32, meetings: Vec<Vec<i32>>) -> i32 {
-        let mut pre_sums = vec![0; (days + 2) as usize];
+    fn count_days(days: i32, meetings: Vec<Vec<i32>>) -> i32 {
+        let mut events: Vec<(i32, i32)> = Vec::with_capacity(meetings.len() * 2);
 
         for meeting in meetings {
-            let start = meeting[0] as usize;
-            let end = meeting[1] as usize;
+            let start = meeting[0];
+            let end = meeting[1];
 
-            pre_sums[start] -= 1;
-            pre_sums[end + 1] += 1;
+            events.push((start, -1));
+            events.push((end + 1, 1));
         }
 
-        let mut count = -2;
-        let mut sum = 0;
-        for pre_sum in pre_sums {
-            sum += pre_sum;
+        events.sort_unstable_by_key(|&(idx, _)| idx);
 
-            if sum >= 0 {
-                count += 1;
+        let mut count = 0;
+        let mut cur = 1;
+        let mut active = 0;
+
+        for event in events {
+            let (idx, offset) = event;
+
+            if idx > cur {
+                if active == 0 {
+                    count += idx - cur;
+                }
+                cur = idx;
             }
+
+            // idx == cur
+            active += offset;
+        }
+
+        if cur <= days {
+            count += days + 1 - cur;
         }
 
         count
