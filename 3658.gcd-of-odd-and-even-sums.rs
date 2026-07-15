19f550733655d82af122e9e7b2ab300d954ec383
ACY222 <838113915@qq.com>
2026-07-15 23:01:50 +0800

    3658 v1.0 interesting
    Perf: 100 82

---
+++ 3658.gcd-of-odd-and-even-sums.rs
@@ -0,0 +1,13 @@
+// @leet start
+impl Solution {
+    pub fn gcd_of_odd_even_sums(n: i32) -> i32 {
+        // let sum_odd = n * n;
+        // let sum_even = (1 + n) * n;
+        //
+        // // gcd(n * n, n(n + 1))
+        // // = n * gcd(n, n + 1) = n * 1
+        // gcd(sum_odd, sum_even)
+        n
+    }
+}
+// @leet end
