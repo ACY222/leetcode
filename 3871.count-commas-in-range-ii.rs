1afe24c3f39c8752bf5f6c5644c1688d639d7560
ACY222 <838113915@qq.com>
2026-09-09 16:12:41 +0200

    3871 v1.1 a better method
    Perf: 100 29

--- 3871.count-commas-in-range-ii.rs
+++ 3871.count-commas-in-range-ii.rs
@@ -3,24 +3,11 @@ use std::cmp::min;
 impl Solution {
     pub fn count_commas(n: i64) -> i64 {
         let mut count = 0i64;
-        if n >= 1e3 as i64 {
-            count += min(n + 1, 1e6 as i64) - 1e3 as i64;
+        let mut x = 1_000i64;
 
-            if n >= 1e6 as i64 {
-                count += 2 * (min(n + 1, 1e9 as i64) - 1e6 as i64);
-
-                if n >= 1e9 as i64 {
-                    count += 3 * (min(n + 1, 1e12 as i64) - 1e9 as i64);
-
-                    if n >= 1e12 as i64 {
-                        count += 4 * (min(n + 1, 1e15 as i64) - 1e12 as i64);
-
-                        if n == 1e15 as i64 {
-                            count += 5;
-                        }
-                    }
-                }
-            }
+        while n >= x {
+            count += (n - x) + 1;
+            x *= 1_000;
         }
 
         count
