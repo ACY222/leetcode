5ca584bc9b81d26ad79cca2bcf877f16031b7d1a
ACY222 <838113915@qq.com>
2026-09-09 16:09:49 +0200

    1984 v1.0 sort_unstable and then check one by one
    Perf: 14 86
    1. sort -> sort_unstable
    2. k as usize

--- 1984.minimum-difference-between-highest-and-lowest-of-k-scores.rs
+++ 1984.minimum-difference-between-highest-and-lowest-of-k-scores.rs
@@ -6,13 +6,12 @@ impl Solution {
         }
 
         let mut nums = nums;
-        nums.sort();
+        let k = k as usize;
+        nums.sort_unstable();
 
         let mut min_diff = i32::MAX;
-        for i in (0..nums.len() + 1 - k as usize) {
-            if nums[i + k as usize - 1] - nums[i] < min_diff {
-                min_diff = nums[i + k as usize - 1] - nums[i];
-            }
+        for i in (0..=nums.len() - k) {
+            min_diff = min_diff.min(nums[i + k - 1] - nums[i]);
         }
 
         min_diff
