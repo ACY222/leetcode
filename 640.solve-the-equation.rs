49e65f1c0b05f6125808d0fa596243800fecb311
ACY222 <838113915@qq.com>
2026-09-21 11:40:29 +0200

    640 v1.1 more elegant implementation
    Perf: 100 93

--- 640.solve-the-equation.rs
+++ 640.solve-the-equation.rs
@@ -4,67 +4,68 @@ impl Solution {
     // variable: x
     // digit
     pub fn solve_equation(equation: String) -> String {
-        let mut coef_x = 0;
-        let mut int = 0;
+        let (left, right) = equation.split_once('=').unwrap();
+        let (x1, const1) = parse(left);
+        let (x2, const2) = parse(right);
 
-        let mut is_zero = false;
-        let mut curr_coef = 0;
-        let mut sign = 1;
-        let mut before_equal = true;
-        for (idx, ch) in equation.chars().enumerate() {
-            match ch {
-                'x' => {
-                    if is_zero {
-                        is_zero = false;
-                        sign = if before_equal { 1 } else { -1 };
-                        continue;
-                    } else {
-                        coef_x += if curr_coef == 0 { sign } else { curr_coef };
-                        curr_coef = 0;
-                        sign = if before_equal { 1 } else { -1 };
-                    }
-                }
-                '=' => {
-                    int += curr_coef;
-                    curr_coef = 0;
-                    before_equal = false;
-                    sign = -1;
-                }
-                '+' => {
-                    int += curr_coef;
-                    curr_coef = 0;
-                    sign = if before_equal { 1 } else { -1 };
-                }
-                '-' => {
-                    int += curr_coef;
-                    curr_coef = 0;
-                    sign = if before_equal { -1 } else { 1 };
-                }
-                digit => {
-                    let digit = digit.to_digit(10).unwrap() as i32;
+        let x = x1 - x2;
+        let constant = const2 - const1;
 
-                    if digit == 0 && curr_coef == 0 {
-                        is_zero = true;
-                        continue;
-                    }
-                    curr_coef = curr_coef * 10 + sign * digit;
-
-                    if idx == equation.len() - 1 {
-                        int += curr_coef;
-                        curr_coef = 0;
-                    }
-                }
+        if x == 0 {
+            if constant == 0 {
+                return "Infinite solutions".to_string();
+            } else {
+                return "No solution".to_string();
             }
-            is_zero = false;
-        }
-
-        if coef_x == 0 && int != 0 {
-            return "No solution".to_string();
-        } else if coef_x == 0 && int == 0 {
-            return "Infinite solutions".to_string();
         } else {
-            return format!("x={}", -int / coef_x);
+            return format!("x={}", constant / x);
         }
     }
+}
+
+fn parse(s: &str) -> (i32, i32) {
+    let mut x = 0;
+    let mut constant = 0;
+
+    let bytes = s.as_bytes();
+
+    let mut idx = 0;
+
+    while idx < bytes.len() {
+        let mut sign = 1;
+
+        // + -
+        if bytes[idx] == b'+' {
+            idx += 1;
+        } else if bytes[idx] == b'-' {
+            idx += 1;
+            sign = -1;
+        }
+
+        let mut has_num = false;
+        let mut num = 0;
+
+        // digits
+        while idx < bytes.len() && bytes[idx].is_ascii_digit() {
+            num = num * 10 + (bytes[idx] - b'0') as i32;
+            idx += 1;
+            has_num = true;
+        }
+
+        // no digits any more
+        if idx < bytes.len() && bytes[idx] == b'x' {
+            // x / -x / +x
+            if !has_num {
+                num = 1;
+            }
+
+            x += num * sign;
+            idx += 1;
+        } else {
+            constant += num * sign;
+        }
+    }
+
+    (x, constant)
 }
 // @leet end
