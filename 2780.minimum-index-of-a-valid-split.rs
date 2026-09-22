ffdf9c0a71f085c3801949325630be235269a25c
ACY222 <838113915@qq.com>
2026-09-22 11:29:31 +0200

    2780 v2.0 a method based on Boyer–Moore Majority Vote learnt from chatgpt
    Perf: 100 100

--- 2780.minimum-index-of-a-valid-split.rs
+++ 2780.minimum-index-of-a-valid-split.rs
@@ -2,41 +2,36 @@
 use std::collections::HashMap;
 impl Solution {
     pub fn minimum_index(nums: Vec<i32>) -> i32 {
-        let mut left_freq = HashMap::new();
-        let mut right_freq = HashMap::new();
-        let mut last_dom = nums[0];
-        let mut curr_dom;
-        let mut idx = 0;
+        let mut candidate = 0;
+        let mut count = 0;
 
-        for &num in nums.iter() {
-            right_freq
-                .entry(num)
-                .and_modify(|freq| *freq += 1)
-                .or_insert(1);
-        }
-
-        while idx < nums.len() - 1 {
-            right_freq.entry(nums[idx]).and_modify(|freq| *freq -= 1);
-            left_freq
-                .entry(nums[idx])
-                .and_modify(|freq| *freq += 1)
-                .or_insert(1);
-
-            if last_dom != 0 && left_freq[&last_dom] * 2 > idx + 1 {
-                curr_dom = last_dom;
-            } else if left_freq[&nums[idx]] * 2 > idx + 1 {
-                curr_dom = nums[idx];
+        for &num in &nums {
+            if count == 0 {
+                candidate = num;
+                count = 1;
+            } else if candidate == num {
+                count += 1;
             } else {
-                curr_dom = 0;
+                count -= 1;
             }
-
-            if curr_dom != 0 && right_freq.get(&curr_dom).unwrap_or(&0) * 2 > nums.len() - idx - 1 {
-                return idx as i32;
-            }
-
-            last_dom = curr_dom;
-            idx += 1;
         }
+
+        let total = nums.iter().filter(|&&num| num == candidate).count();
+
+        let mut left_count = 0;
+        let n = nums.len();
+
+        for i in 0..n - 1 {
+            if nums[i] == candidate {
+                left_count += 1;
+            }
+
+            let right_count = total - left_count;
+            if left_count * 2 > i + 1 && right_count * 2 > n - 1 - i {
+                return i as i32;
+            }
+        }
+
         -1
     }
 }
