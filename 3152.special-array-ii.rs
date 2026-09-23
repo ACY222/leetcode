94ba7eed725202f1f43726b6b2a244b4c9ae0e69
ACY222 <838113915@qq.com>
2026-09-23 16:13:02 +0200

    3152. v1.0 simple idea, simple performance
    Perf: 40 20

---
+++ 3152.special-array-ii.rs
@@ -0,0 +1,46 @@
+// @leet start
+impl Solution {
+    pub fn is_array_special(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<bool> {
+        let len = nums.len();
+        let mut special_arrays = Vec::new();
+        let mut idx = 0;
+
+        while idx < len - 1 {
+            // is special pair
+            if (nums[idx] - nums[idx + 1]) % 2 != 0 {
+                let from = idx as i32;
+
+                while idx < len - 1 && (nums[idx] - nums[idx + 1]) % 2 != 0 {
+                    idx += 1;
+                }
+
+                let to = idx as i32;
+                special_arrays.push((from, to));
+            }
+
+            idx += 1;
+        }
+
+        let mut results = Vec::new();
+        results.reserve(queries.len());
+        for query in queries {
+            let from = query[0];
+            let to = query[1];
+
+            if from == to {
+                results.push(true);
+                continue;
+            }
+
+            let idx = special_arrays.partition_point(|&(start, _)| start <= from);
+            if idx > 0 && special_arrays[idx - 1].1 >= to {
+                results.push(true);
+            } else {
+                results.push(false);
+            }
+        }
+
+        results
+    }
+}
+// @leet end
